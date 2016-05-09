318d94dba7fde1c0bb5ecd93c7d5b8dda41729f2
viile <dakgzhu@foxmail.com>
2016-05-10 01:58:39 +0800

    update

--- source/app.d
+++ source/app.d
@@ -8,4 +8,9 @@ void main()
 	writeln("Edit source/app.d to start your project.");
     auto tlate = compile_temple!"foo, bar, baz";
     writeln(tlate.toString()); // Prints "foo, bar, baz"
+
+    auto dhtml = compile_temple_file!"system/hello.dhtml";
+    auto context = new TempleContext();
+    context.hour = 5;
+    dhtml.render(stdout);
 }
