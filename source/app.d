d87e5fc30b4381d73d3c9df94f6b2a4f8ddea33c
viile <dakgzhu@foxmail.com>
2016-05-09 23:36:28 +0800

    add hunt view

--- source/app.d
+++ source/app.d
@@ -1,6 +1,11 @@
-import std.stdio;
+import 
+  std.stdio,
+  std.string,
+  hunt.view;
 
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
+    auto tlate = compile_temple!"foo, bar, baz";
+    writeln(tlate.toString()); // Prints "foo, bar, baz"
 }
