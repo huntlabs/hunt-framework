5227fdbf64fc57aedafb602696ea4fe63e3f7457
渡世白玉 <dushibaiyu@yahoo.com>
2016-05-11 23:51:57 +0800

    fmt code

--- source/hunt/router/middleware.d
+++ source/hunt/router/middleware.d
@@ -20,112 +20,137 @@ final class PipelineImpl(REQ, RES)
     alias Context = ContextImpl!(REQ, RES);
     alias HandleDelegate = void delegate(REQ, RES);
     alias Pipeline = PipelineImpl!(REQ, RES);
-    
+
     this()
     {
         _first = new Context();
         _last = frist;
     }
+
     ~this()
     {
         _first.destroy;
         _last = null;
     }
-    
-    void  addHandler(MiddleWare hander)
-    in{assert(hander);}
-    body{
-	Context cxt = new Context(hander);
+
+    void addHandler(MiddleWare hander)
+    in
+    {
+        assert(hander);
+    }
+    body
+    {
+        Context cxt = new Context(hander);
         _last._next = cxt;
     }
-    
+
     void addHandler(HandleDelegate handler)
-    in{assert(hander);}
-    body{
+    in
+    {
+        assert(hander);
+    }
+    body
+    {
         Context cxt = new Context(new DelegateMiddleWare(handler));
         _last._next = cxt;
     }
-    
+
     void append(Pipeline pipeline)
     {
-        if(pipeline is null) return;
-        if(pipeline.empty()) return;
+        if (pipeline is null)
+            return;
+        if (pipeline.empty())
+            return;
         Context frist = pipeline._first._next;
-        while(frist !is null)
+        while (frist !is null)
         {
             _last._next = frist;
             frist = frist._next;
         }
     }
-    
+
     void handleActive(REQ req, RES res)
     {
-        _first.next(forward!(req,res));
+        _first.next(forward!(req, res));
     }
 
     @property bool empty()
     {
-	if(_first == _last)
-	    return true;
-	else
+        if (_first == _last)
+            return true;
+        else
             return false;
     }
-    
-    @property _matchData(){return _matchData;}
-    
+
+    @property _matchData()
+    {
+        return _matchData;
+    }
+
     void addMatch(string key, string value)
     {
         _matchData[key] = value;
     }
-    
+
 private:
     Context _first = null;
     Context _last = null;
     string[string] _matchData;
 }
 
-
 final class ContextImpl(REQ, RES)
 {
     alias MiddleWare = IMiddleWare!(REQ, RES);
     alias Context = ContextImpl!(REQ, RES);
 
-    this(){}
-    this(MiddleWare handler){_middleWare = handler;}
-    ~this(){_next = null;_middleWare = null;}
-    
+    this()
+    {
+    }
+
+    this(MiddleWare handler)
+    {
+        _middleWare = handler;
+    }
+
+    ~this()
+    {
+        _next = null;
+        _middleWare = null;
+    }
+
     void next(REQ req, RES res)
     {
-        if(_next)
-            _next.handle(forward!(req,res));
+        if (_next)
+            _next.handle(forward!(req, res));
     }
+
 private:
     void handle(REQ req, RES res)
     {
-        if(_middleWare)
-            _middleWare.handle(this,req,res);
+        if (_middleWare)
+            _middleWare.handle(this, req, res);
     }
-    
-    MiddleWare  _middleWare = null;
+
+    MiddleWare _middleWare = null;
     Context _next = null;
 }
 
 private:
-    final class DelegateMiddleWare(REQ,RES) :  IMiddleWare!(REQ,RES)
+final class DelegateMiddleWare(REQ, RES) : IMiddleWare!(REQ, RES)
+{
+    alias HandleDelegate = void delegate(REQ, RES);
+
+    this(HandleDelegate handle)
     {
-        alias HandleDelegate = void delegate(REQ, RES);
-        
-        this(HandleDelegate handle)
-        {
-            _handler = handle;
-        }
-        
-        override void handle(Context ctx, REQ req, RES res)
-        {
-            _handler(req,res);
-            ctx.next(forward!(req,res));
-        }
-        
-    private:
-        HandleDelegate _handler;
+        _handler = handle;
     }
+
+    override void handle(Context ctx, REQ req, RES res)
+    {
+        _handler(req, res);
+        ctx.next(forward!(req, res));
+    }
+
+private:
+    HandleDelegate _handler;
+}
--- source/hunt/router/router.d
+++ source/hunt/router/router.d
@@ -5,7 +5,6 @@ import std.regex;
 import hunt.router.middleware;
 import hunt.router.utils;
 
-
 final shared class Router(REQ, RES)
 {
     alias HandleDelegate = void delegate(REQ, RES);
@@ -13,47 +12,67 @@ final shared class Router(REQ, RES)
     alias PipelineFactory = IPipelineFactory!(REQ, RES);
     alias RouterElement = PathElement!(REQ, RES);
     alias RouterMap = ElementMap!(REQ, RES);
-    
-    void setGlobalBeforePipelineFactory(PipelineFactory before){_gbefore = before;}
-    void setGlobalAfterPipelineFactory(PipelineFactory after){_gafter = after;}
-    
-    RouterElement addRouter(string method, string path, HandleDelegate handle, 
-                            shared PipelineFactory before = null, shared PipelineFactory after = null)
+
+    void setGlobalBeforePipelineFactory(PipelineFactory before)
     {
-        if(condigDone) return null;
-        auto map = _map.get(method,null);
-        if(!map)
+        _gbefore = before;
+    }
+
+    void setGlobalAfterPipelineFactory(PipelineFactory after)
+    {
+        _gafter = after;
+    }
+
+    RouterElement addRouter(string method, string path, HandleDelegate handle,
+        shared PipelineFactory before = null, shared PipelineFactory after = null)
+    {
+        if (condigDone)
+            return null;
+        auto map = _map.get(method, null);
+        if (!map)
         {
             map = new RouterMap(this);
             _map[method] = map;
         }
-        map.add(path,handle,before,after);
+        map.add(path, handle, before, after);
     }
-    
-    Pipeline match(string method,string path)  
+
+    Pipeline match(string method, string path)
     {
-        if(!condigDone) return null;
-        auto map = _map.get(method,null);
-        if(!map) return null;
+        if (!condigDone)
+            return null;
+        auto map = _map.get(method, null);
+        if (!map)
+            return null;
         return map.match(path);
     }
-    
-    Pipeline getGlobalBrforeMiddleware()  
+
+    Pipeline getGlobalBrforeMiddleware()
     {
-        if(_gbefore) return _gbefore.newPipeline();
-        else return null;
+        if (_gbefore)
+            return _gbefore.newPipeline();
+        else
+            return null;
     }
-    
-    Pipeline getGlobalAfterMiddleware()  
+
+    Pipeline getGlobalAfterMiddleware()
     {
-        if(_gafter) return _gafter.newPipeline();
-        else return null;
+        if (_gafter)
+            return _gafter.newPipeline();
+        else
+            return null;
     }
-    
-    @property bool condigDone(){return _configDone;}
-    
-    void done(){_configDone = true;}
-    
+
+    @property bool condigDone()
+    {
+        return _configDone;
+    }
+
+    void done()
+    {
+        _configDone = true;
+    }
+
 private:
     PipelineFactory _gbefore = null;
     PipelineFactory _gafter = null;
@@ -61,31 +80,63 @@ private:
     bool _configDone = false;
 }
 
-
 shared class PathElement(REQ, RES)
 {
     alias HandleDelegate = void delegate(REQ, RES);
     alias Pipeline = PipelineImpl!(REQ, RES);
     alias PipelineFactory = Pipeline delegate();
 
-    this(string path){_path = path;}
-    
-    PathElement!(REQ, RES) macth(string path,Pipeline pipe) 
+    this(string path)
+    {
+        _path = path;
+    }
+
+    PathElement!(REQ, RES) macth(string path, Pipeline pipe)
     {
         return this;
     }
 
-    final @property path() const {return _path;} 
-    
-    final @property handler() const {return _handler;}
-    final @property handler(HandleDelegate handle){ _handler = handle ;}
-    
-    final Pipeline getBeforeMiddleware()  {if(_before) return _before.newPipeline(); else return null;}
-    final Pipeline getAfterMiddleware()  {if(_after) return _after.newPipeline(); else return null;}
-    
-    final void setBeforePipelineFactory(shared PipelineFactory before){_before = before;}
-    final void setAfterPipelineFactory(shared PipelineFactory after){_after = after;}
-    
+    final @property path() const
+    {
+        return _path;
+    }
+
+    final @property handler() const
+    {
+        return _handler;
+    }
+
+    final @property handler(HandleDelegate handle)
+    {
+        _handler = handle;
+    }
+
+    final Pipeline getBeforeMiddleware()
+    {
+        if (_before)
+            return _before.newPipeline();
+        else
+            return null;
+    }
+
+    final Pipeline getAfterMiddleware()
+    {
+        if (_after)
+            return _after.newPipeline();
+        else
+            return null;
+    }
+
+    final void setBeforePipelineFactory(shared PipelineFactory before)
+    {
+        _before = before;
+    }
+
+    final void setAfterPipelineFactory(shared PipelineFactory after)
+    {
+        _after = after;
+    }
+
 private:
     string _path;
     HandleDelegate _handler = null;
@@ -95,69 +146,72 @@ private:
 
 private:
 
-final shared class RegexElement(REQ, RES) : PathElement!(REQ,RES)
+final shared class RegexElement(REQ, RES) : PathElement!(REQ, RES)
 {
     this(string path)
     {
         super(path);
     }
-    
-    override PathElement!(REQ, RES) macth(string path,Pipeline pipe) 
+
+    override PathElement!(REQ, RES) macth(string path, Pipeline pipe)
     {
         auto rg = regex(compiledRoute.regex, "s");
         auto mt = matchFirst(path, _reg);
-        if(!mt)
+        if (!mt)
         {
-                return null;
+            return null;
         }
-        foreach(attr;_reg.namedCaptures)
+        foreach (attr; _reg.namedCaptures)
         {
-            pipe.addMatch(attr,mt[attr]);
+            pipe.addMatch(attr, mt[attr]);
         }
         return this;
     }
-    
+
 private:
     bool setRegex(string regex)
     {
-        if(regex.length == 0) return false;
+        if (regex.length == 0)
+            return false;
         _reg = buildRegex(regex);
-        if(_reg.length == 0) return false;
+        if (_reg.length == 0)
+            return false;
         return true;
     }
+
     string _reg;
 }
 
-
-final shared  class RegexMap(REQ, RES) 
+final shared class RegexMap(REQ, RES)
 {
     alias RElement = RegexElement!(REQ, RES);
     alias PElement = PathElement!(REQ, RES);
     alias RElementMap = RegexMap!(REQ, RES);
     alias Pipeline = PipelineImpl!(REQ, RES);
-    
+
     this(string path)
     {
         super(path);
     }
-    
+
     PElement add(RElement ele, string preg)
     {
         string rege;
         string str = getFristPath(preg, rege);
-        if(str.length == 0)
+        if (str.length == 0)
         {
             ele.destroy;
             return null;
         }
-        if(isHaveRegex(str))
+        if (isHaveRegex(str))
         {
-            if(!ele.setRegex(preg)) return null;
-            
+            if (!ele.setRegex(preg))
+                return null;
+
             bool isHas = false;
-            for(int i = 0; i < _list.length; ++i) //添加的时候去重
+            for (int i = 0; i < _list.length; ++i) //添加的时候去重
             {
-                if(_list[i].path == ele.path)
+                if (_list[i].path == ele.path)
                 {
                     isHas = true;
                     auto tele = _list[i];
@@ -165,7 +219,7 @@ final shared  class RegexMap(REQ, RES)
                     tele.destroy;
                 }
             }
-            if(!isHas)
+            if (!isHas)
             {
                 _list ~= ele;
             }
@@ -173,32 +227,35 @@ final shared  class RegexMap(REQ, RES)
         }
         else
         {
-            RElementMap map = _map.get(str,null);
-            if(!map){
+            RElementMap map = _map.get(str, null);
+            if (!map)
+            {
                 map = new RElementMap(str);
                 _map[str] = map;
             }
-            return map.add(ele,rege);
+            return map.add(ele, rege);
         }
     }
-    
-    PElement macth(string path,Pipeline pipe) 
+
+    PElement macth(string path, Pipeline pipe)
     {
-        if(path.length == 0) return null;
+        if (path.length == 0)
+            return null;
         string lpath;
-        string frist = getFristPath(path,lpath);
-        if(frist.length == 0) return null;
-        RElementMap map = _map.get(frist,null);
-        if(map)
+        string frist = getFristPath(path, lpath);
+        if (frist.length == 0)
+            return null;
+        RElementMap map = _map.get(frist, null);
+        if (map)
         {
-            return map.macth(lpath,pipe);
+            return map.macth(lpath, pipe);
         }
         else
         {
-            foreach(ele;RElement)
+            foreach (ele; RElement)
             {
-                auto element = ele.macth(path,pipe);
-                if(element)
+                auto element = ele.macth(path, pipe);
+                if (element)
                 {
                     return element;
                 }
@@ -206,12 +263,11 @@ final shared  class RegexMap(REQ, RES)
         }
         return null;
     }
-    
+
 private:
     RElementMap[string] _map;
     RElement[] _list;
 }
-
 
 final class ElementMap(REQ, RES)
 {
@@ -219,26 +275,26 @@ final class ElementMap(REQ, RES)
     alias PElement = PathElement!(REQ, RES);
     alias RElementMap = RegexMap!(REQ, RES);
     alias Pipeline = PipelineImpl!(REQ, RES);
-    alias Route =  Router!(REQ, RES);
+    alias Route = Router!(REQ, RES);
     alias HandleDelegate = void delegate(REQ, RES);
     alias Pipeline = PipelineImpl!(REQ, RES);
     alias PipelineFactory = Pipeline delegate();
-    
+
     this(Route router)
     {
         _router = router;
         _regexMap = new RElementMap("/");
     }
-    
-    PElement add(string path,HandleDelegate handle,PipelineFactory before,PipelineFactory after)
+
+    PElement add(string path, HandleDelegate handle, PipelineFactory before, PipelineFactory after)
     {
-        if(isHaveRegex(path))
+        if (isHaveRegex(path))
         {
             auto ele = new RElement(path);
             ele.setAfterPipelineFactory(after);
             ele.setBeforePipelineFactory(before);
             ele.handler = handle;
-            _regexMap.add(ele,path);
+            _regexMap.add(ele, path);
         }
         else
         {
@@ -249,35 +305,35 @@ final class ElementMap(REQ, RES)
             _pathMap[path] = ele;
         }
     }
-    
+
     Pipeline macth(string path)
     {
         Pipeline pipe = new Pipeline();
-        
-        PElement element = _pathMap.get(path,null);
-        
-        if(!element)
+
+        PElement element = _pathMap.get(path, null);
+
+        if (!element)
         {
-            element =  _regexMap.macth(path,pipe);
+            element = _regexMap.macth(path, pipe);
         }
-        
-        if(element)
+
+        if (element)
         {
             pipe.append(_router.getGlobalBrforeMiddleware());
             pipe.append(element.getBeforeMiddleware());
             pipe.addHander(element.handler);
             pipe.append(element.getAfterMiddleware());
             pipe.append(_router.getGlobalAfterMiddleware());
-            
+
         }
-        else 
+        else
         {
             pipe.destroy;
             pipe = null;
         }
         return pipe;
     }
-    
+
 private:
     Route _router;
     PElement[string] _pathMap;
