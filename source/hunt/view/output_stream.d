8250edfbbc7afef20a90d3c18881ed7ed1a00a08
viile <dakgzhu@foxmail.com>
2016-05-13 00:05:09 +0800

    update

--- source/hunt/view/output_stream.d
+++ source/hunt/view/output_stream.d
@@ -19,64 +19,69 @@
 
 module hunt.view.output_stream;
 
-private {
-	import std.range;
-	import std.stdio;
+private
+{
+    import std.range;
+    import std.stdio;
 }
 
 // Wraps any generic output stream/sink
-struct TempleOutputStream {
+struct TempleOutputStream
+{
 private:
-	//void delegate(string) scope_sink;
-	void delegate(string) sink;
+    //void delegate(string) scope_sink;
+    void delegate(string) sink;
 
 public:
-	this(T)(ref T os)
-	if(isOutputRange!(T, string)) {
-		this.sink = delegate(str) {
-			os.put(str);
-		};
-	}
+    this(T)(ref T os) if (isOutputRange!(T, string))
+    {
+        this.sink = delegate(str) { os.put(str); };
+    }
 
-	this(ref File f) {
-		this.sink = delegate(str) {
-			f.write(str);
-		};
-	}
+    this(ref File f)
+    {
+        this.sink = delegate(str) { f.write(str); };
+    }
 
-	this(void delegate(string) s) {
-		this.sink = s;
-	}
+    this(void delegate(string) s)
+    {
+        this.sink = s;
+    }
 
-	this(void function(string) s) {
-		this.sink = delegate(str) {
-			s(str);
-		};
-	}
+    this(void function(string) s)
+    {
+        this.sink = delegate(str) { s(str); };
+    }
 
-	void put(string s) {
-		this.sink(s);
-	}
+    void put(string s)
+    {
+        this.sink(s);
+    }
 
-	// for vibe.d's html escape
-	// TODO: write own html escaping mechanism, as this one requires an allocation
-	// made for each char written to the output
-	void put(dchar d) {
-		import std.conv;
-		this.sink(d.to!string);
-	}
+    // for vibe.d's html escape
+    // TODO: write own html escaping mechanism, as this one requires an allocation
+    // made for each char written to the output
+    void put(dchar d)
+    {
+        import std.conv;
 
-	invariant() {
-		assert(this.sink !is null);
-	}
+        this.sink(d.to!string);
+    }
+
+    invariant()
+    {
+        assert(this.sink !is null);
+    }
 }
 
 // newtype struct
-struct TempleInputStream {
-	// when called, 'into' pipes its output into OutputStream
-	void delegate(ref TempleOutputStream os) into;
+struct TempleInputStream
+{
+    // when called, 'into' pipes its output into OutputStream
+    void delegate(ref TempleOutputStream os) into;
 
-	invariant() {
-		assert(this.into !is null);
-	}
+    invariant()
+    {
+        assert(this.into !is null);
+    }
 }
--- source/hunt/view/package.d
+++ source/hunt/view/package.d
@@ -19,70 +19,69 @@
 
 module hunt.view;
 
-public import
-	hunt.view.util,
-	hunt.view.delims,
-	hunt.view.func_string_gen;
+public import hunt.view.util, hunt.view.delims, hunt.view.func_string_gen;
 public import std.array : appender, Appender;
 public import std.range : isOutputRange;
 public import std.typecons : scoped;
 public import std.stdio;
-public {
+
+public
+{
     import hunt.view.temple_context : TempleContext;
-	import hunt.view.output_stream  : TempleOutputStream, TempleInputStream;
+    import hunt.view.output_stream : TempleOutputStream, TempleInputStream;
 }
 
 /**
  * Temple
  * Main template for generating Temple functions
  */
-CompiledTemple compile_temple(string __TempleString, __Filter = void, uint line = __LINE__, string file = __FILE__)()
+CompiledTemple compile_temple(string __TempleString, __Filter = void,
+        uint line = __LINE__, string file = __FILE__)()
 {
     import std.conv : to;
-    return compile_temple!(__TempleString, file~":"~line.to!string ~ ": InlineTemplate", __Filter);
+
+    return compile_temple!(__TempleString, file ~ ":" ~ line.to!string ~ ": InlineTemplate",
+            __Filter);
 }
-deprecated("Please use compile_temple")
-auto Temple(ARGS...)() {
+
+deprecated("Please use compile_temple") auto Temple(ARGS...)()
+{
     return .compile_temple!(ARGS)();
 }
 
-public
-CompiledTemple compile_temple(
-	string __TempleString,
-	string __TempleName,
-	__Filter = void)()
+public CompiledTemple compile_temple(string __TempleString, string __TempleName, __Filter = void)()
 {
-	// __TempleString: The template string to compile
-	// __TempleName: The template's file name, or 'InlineTemplate'
-	// __Filter: FP for the rendered template
+    // __TempleString: The template string to compile
+    // __TempleName: The template's file name, or 'InlineTemplate'
+    // __Filter: FP for the rendered template
 
-	// Is a Filter present?
-	enum __TempleHasFP = !is(__Filter == void);
+    // Is a Filter present?
+    enum __TempleHasFP = !is(__Filter == void);
 
-	// Needs to be kept in sync with the param name of the Filter
-	// passed to Temple
-	enum __TempleFilterIdent = __TempleHasFP ? "__Filter" : "";
+    // Needs to be kept in sync with the param name of the Filter
+    // passed to Temple
+    enum __TempleFilterIdent = __TempleHasFP ? "__Filter" : "";
 
-	// Generates the actual function string, with the function name being
-	// `TempleFunc`.
-	const __TempleFuncStr = __temple_gen_temple_func_string(
-		__TempleString,
-		__TempleName,
-		__TempleFilterIdent);
+    // Generates the actual function string, with the function name being
+    // `TempleFunc`.
+    const __TempleFuncStr = __temple_gen_temple_func_string(__TempleString,
+            __TempleName, __TempleFilterIdent);
 
-	//pragma(msg, __TempleFuncStr);
+    //pragma(msg, __TempleFuncStr);
 
-	mixin(__TempleFuncStr);
-	//pragma(msg, "TempleFunc ", __TempleFuncStr, "...");
+    mixin(__TempleFuncStr);
+    //pragma(msg, "TempleFunc ", __TempleFuncStr, "...");
 
-	static if(__TempleHasFP) {
-		alias temple_func = TempleFunc!__Filter;
-	}
-	else {
-		alias temple_func = TempleFunc;
-	}
+    static if (__TempleHasFP)
+    {
+        alias temple_func = TempleFunc!__Filter;
+    }
+    else
+    {
+        alias temple_func = TempleFunc;
+    }
 
-	return CompiledTemple(&temple_func, null);
+    return CompiledTemple(&temple_func, null);
 }
 
 /**
@@ -92,18 +91,23 @@ CompiledTemple compile_temple(
  */
 CompiledTemple compile_temple_file(string template_file, Filter = void)()
 {
-	pragma(msg, "Compiling ", template_file, "...");
-	return compile_temple!(import(template_file), template_file, Filter);
+    pragma(msg, "Compiling ", template_file, "...");
+    return compile_temple!(import(template_file), template_file, Filter);
 }
 
+CompiledTemple display(string template_file)()
+{
+    return compile_temple!(import(template_file), template_file);
+}
+
+/*
 string display(string template_file)()
 {
 	auto temple = compile_temple!(import(template_file), template_file);
     auto context = new TempleContext();
     context.name = "owner";
-    return temple.toString(context);
+    return temple.toString();
 }
-
 string display(string template_file,string[string] params)()
 {
 	auto temple = compile_temple!(import(template_file), template_file);
@@ -111,12 +115,12 @@ string display(string template_file,string[string] params)()
     foreach(key,value;params){
         context.opIndexAssign(key,value);
     }
-    //context.name = "owner";
     return temple.toString(context);
 }
+*/
 
-deprecated("Please use compile_temple_file")
-auto TempleFile(ARGS...)() {
+deprecated("Please use compile_temple_file") auto TempleFile(ARGS...)()
+{
     return .compile_temple_file!(ARGS)();
 }
 
@@ -124,23 +128,27 @@ auto TempleFile(ARGS...)() {
  * TempleFilter
  * Curries a Temple to always use a given template filter, for convienence
  */
-template TempleFilter(Filter) {
-    template compile_temple(ARGS...) {
+template TempleFilter(Filter)
+{
+    template compile_temple(ARGS...)
+    {
         alias compile_temple = .compile_temple!(ARGS, Filter);
     }
-    template compile_temple_file(ARGS...) {
+
+    template compile_temple_file(ARGS...)
+    {
         alias compile_temple_file = .compile_temple_file!(ARGS, Filter);
     }
 
-    deprecated("Please compile_temple")      alias Temple     = compile_temple;
+    deprecated("Please compile_temple") alias Temple = compile_temple;
     deprecated("Please compile_temple_file") alias TempleFile = compile_temple_file;
 }
 
 /**
  * CompiledTemple
  */
-package
-struct CompiledTemple {
+package struct CompiledTemple
+{
 
 package:
     alias TempleFuncSig = void function(TempleContext);
@@ -150,8 +158,12 @@ package:
     const(CompiledTemple)* partial_rendr = null;
 
     this(TempleFuncSig rf, const(CompiledTemple*) cf)
-    in { assert(rf); }
-    body {
+    in
+    {
+        assert(rf);
+    }
+    body
+    {
         this.render_func = rf;
         this.partial_rendr = cf;
     }
@@ -160,15 +172,17 @@ public:
     //deprecated static Temple opCall()
 
     // render template directly to a string
-    string toString(TempleContext tc = null) const {
+    string toString(TempleContext tc = null) const
+    {
         auto a = appender!string();
         this.render(a, tc);
         return a.data;
     }
 
     // render using an arbitrary output range
-    void render(T)(ref T os, TempleContext tc = null) const {
-        if(	isOutputRange!(T, string) && !is(T == TempleOutputStream))
+    void render(T)(ref T os, TempleContext tc = null) const
+    {
+        if (isOutputRange!(T, string) && !is(T == TempleOutputStream))
         {
             auto oc = TempleOutputStream(os);
             return render(oc, tc);
@@ -176,23 +190,28 @@ public:
     }
 
     // render using a sink function (DMD can't seem to cast a function to a delegate)
-    void render(void delegate(string) sink, TempleContext tc = null) const {
-    	auto oc = TempleOutputStream(sink);
-    	this.render(oc, tc); 
+    void render(void delegate(string) sink, TempleContext tc = null) const
+    {
+        auto oc = TempleOutputStream(sink);
+        this.render(oc, tc);
     }
-    void render(void function(string) sink, TempleContext tc = null) const {
-    	auto oc = TempleOutputStream(sink);
-    	this.render(oc, tc); 
+
+    void render(void function(string) sink, TempleContext tc = null) const
+    {
+        auto oc = TempleOutputStream(sink);
+        this.render(oc, tc);
     }
+
     import std.stdio;
-    void render(ref std.stdio.File f, TempleContext tc = null) const {
+
+    void render(ref std.stdio.File f, TempleContext tc = null) const
+    {
         auto oc = TempleOutputStream(f);
         this.render(oc, tc);
     }
 
     // normalized render function, using an TempleOutputStream
-    package
-    void render(ref TempleOutputStream os, TempleContext tc) const
+    package void render(ref TempleOutputStream os, TempleContext tc) const
     {
         // the context never escapes the scope of a template, so it's safe
         // to allocate a new context here
@@ -201,40 +220,49 @@ public:
 
         // and always ensure that a template is passed, at the very least,
         // an empty context (needed for various template scope book keeping)
-        if(tc is null) {
+        if (tc is null)
+        {
             tc = local_tc.Scoped_payload;
             //tc = new TempleContext();
         }
 
         // template renders into given output stream
         auto old = tc.sink;
-        scope(exit) tc.sink = old;
+        scope (exit)
+            tc.sink = old;
         tc.sink = os;
 
         // use the layout if we've got one
-        if(this.partial_rendr !is null) {
+        if (this.partial_rendr !is null)
+        {
             auto old_partial = tc.partial;
 
-                        tc.partial = this.partial_rendr;
-            scope(exit) tc.partial = old_partial;
+            tc.partial = this.partial_rendr;
+            scope (exit)
+                tc.partial = old_partial;
 
             this.render_func(tc);
         }
         // else, call this render function directly
-        else {
+        else
+        {
             this.render_func(tc);
         }
     }
 
     CompiledTemple layout(const(CompiledTemple*) partial) const
-    in {
+    in
+    {
         assert(this.partial_rendr is null, "attempting to set already-set partial of a layout");
     }
-    body {
-    	return CompiledTemple(this.render_func, partial);
+    body
+    {
+        writeln(this.render_func, '\n', partial);
+        return CompiledTemple(this.render_func, partial);
     }
 
-    invariant() {
+    invariant()
+    {
         assert(render_func);
     }
 }
