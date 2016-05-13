377fc1b33c7be4a08cbbd9bd09f063e779270d4d
viile <zhudj@putao.com>
2016-05-13 23:32:53 +0800

    update

--- source/app.d
+++ source/app.d
@@ -15,11 +15,14 @@ void main()
     */
 
     auto parent = compile_temple!"before {{ yield }} after {{ var.name }}";
-    auto child  = compile_temple!"between {{ var.name  }}";
+    auto child  = compile_temple!"{% int test = 1;  %} between {{ var.name  }}   {!test.dhtml!}";
 
     auto composed = parent.layout(&child);
     auto res  = new TempleContext();
     res.name = "viile";
     writeln(composed.toString(res));
     //composed.render(stdout);
+
+
+    writeln(import("test.dhtml"));
 }
