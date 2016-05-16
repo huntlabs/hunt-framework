f5e660fb15a7b4ae7a09fd3b43c7dc18a58b323a
jiyahan <jiyh@putao.com>
2016-05-16 11:20:16 +0800

    format souce code

--- source/hunt/router/routerconfig.d
+++ source/hunt/router/routerconfig.d
@@ -16,16 +16,16 @@ struct RouterContext
     string[] middleWareBefore;
     string[] middleWareAfter;
 }
-class ConfigImpl
+class RouterConfig
 {
-    this(string file_path, string prefix)
+    this(string filePath, string prefix)
     in
     {
-	assert(exists(file_path), "Error file path!");
+	assert(exists(filePath), "Error file path!");
     }
     body
     {
-	_file_path = file_path;
+	_filePath = filePath;
 	_prefix = prefix;
     }
     
@@ -33,13 +33,13 @@ class ConfigImpl
     {
 	
     }
-    RouterContext[] do_parse()
+    RouterContext[] doParse()
     {
 	return this._routerContext;
     }
-    void set_file_path(string file_path)
+    void setFilePath(string filePath)
     {
-	this._file_path = file_path;
+	this._filePath = filePath;
     }
     void set_prefix(string prefix)
     {
@@ -48,126 +48,126 @@ class ConfigImpl
 protected: 
     RouterContext[] _routerContext;
     string _prefix;
-    string _file_path;//文件路径
+    string _filePath;//文件路径
 }
-class ConfigParse : ConfigImpl
+class ConfigParse : RouterConfig 
 {
-    this(string file_path, string prefix=string.init)
+    this(string filePath, string prefix=string.init)
     {
-	super(file_path, prefix);
+	super(filePath, prefix);
     }
 public:
-    override @property RouterContext[] do_parse() 
+    override @property RouterContext[] doParse() 
     {
-	string full_method = "OPTIONS,GET,HEAD,POST,PUT,DELETE,TRACE,CONNECT";
-	string[] output_lines;
-	size_t file_size = cast(size_t) getSize(cast(char[]) _file_path);
-	assert(file_size,"Empty file!");
-	string read_buff;
+	string fullMethod = "OPTIONS,GET,HEAD,POST,PUT,DELETE,TRACE,CONNECT";
+	string[] outputLines;
+	size_t fileSize = cast(size_t) getSize(cast(char[]) _filePath);
+	assert(fileSize,"Empty file!");
+	string readBuff;
 	try{
-	    read_buff = cast(string) read(cast(char[]) _file_path, file_size);
+	    readBuff = cast(string) read(cast(char[]) _filePath, fileSize);
 	}catch(Exception ex){
 	    //assert("Read router config file error!);
 	    throw ex;
 	}
-	string[] line_buffers = splitLines(read_buff);
-	foreach(line; line_buffers)
+	string[] lineBuffers = splitLines(readBuff);
+	foreach(line; lineBuffers)
 	{
 	    if(line != string.init && (line.indexOf('#')<0))
 	    {
-		string[] tmp_splites = splite_by_space(line);
-		RouterContext tmp_route;
-		if(tmp_splites[0] == "*")
-		    tmp_route.method = full_method;
+		string[] tmpSplites = spliteBySpace(line);
+		RouterContext tmpRoute;
+		if(tmpSplites[0] == "*")
+		    tmpRoute.method = fullMethod;
 		else
-		    tmp_route.method = toUpper(tmp_splites[0]);
-		tmp_route.path = tmp_splites[1];
-		tmp_route.hander = parse_to_full_controll(tmp_splites[2]);
-		if(tmp_splites.length == 4)
-		    parse_middleware(tmp_splites[3], tmp_route.middleWareBefore, tmp_route.middleWareAfter);
-		_routerContext ~= tmp_route;
+		    tmpRoute.method = toUpper(tmpSplites[0]);
+		tmpRoute.path = tmpSplites[1];
+		tmpRoute.hander = parseToFullControll(tmpSplites[2]);
+		if(tmpSplites.length == 4)
+		    parseMiddleware(tmpSplites[3], tmpRoute.middleWareBefore, tmpRoute.middleWareAfter);
+		_routerContext ~= tmpRoute;
 	    }
 	}
 	return _routerContext;
     }
-    void set_controller_prefix(string controller_prefix)
+    void setControllerPrefix(string controllerPrefix)
     {
-	_controller_prefix = controller_prefix;
+	_controllerPrefix = controllerPrefix;
     }
-    void set_before_flag(string before_flag)
+    void setBeforeFlag(string beforeFlag)
     {
-	_before_flag = before_flag;
+	_beforeFlag = beforeFlag;
     }
-    void set_after_flag(string after_flag)
+    void setAfterFlag(string afterFlag)
     {
-	_after_flag = after_flag;
+	_afterFlag = afterFlag;
     }
 private:
-    string parse_to_full_controll(string in_buff)
+    string parseToFullControll(string inBuff)
     {
-	string[] sprit_arr = split(in_buff, '/');
-	assert(sprit_arr.length > 1, "whitout /");
+	string[] spritArr = split(inBuff, '/');
+	assert(spritArr.length > 1, "whitout /");
 	string output;
 	if(_prefix)
 	{
-	    assert(sprit_arr.length == 4, "Wrong controller config!");
-	    output ~= sprit_arr[0]~"."~sprit_arr[1]~"."~to!string(sprit_arr[2].asCapitalized)~_controller_prefix~"."~sprit_arr[3];
+	    assert(spritArr.length == 4, "Wrong controller config!");
+	    output ~= spritArr[0]~"."~spritArr[1]~"."~to!string(spritArr[2].asCapitalized)~_controllerPrefix~"."~spritArr[3];
 	}
 	else
 	{
-	    assert(sprit_arr.length == 3, "Wrong controller config!");
-	    output ~= "application"~"."~sprit_arr[0]~"."~to!string(sprit_arr[1].asCapitalized)~_controller_prefix~"."~sprit_arr[2];
+	    assert(spritArr.length == 3, "Wrong controller config!");
+	    output ~= "application"~"."~spritArr[0]~"."~to!string(spritArr[1].asCapitalized)~_controllerPrefix~"."~spritArr[2];
 	}
 
 	return output;
     }
-    string[] splite_by_space(string in_buff)
+    string[] spliteBySpace(string inBuff)
     {
 	auto r = regex(r"\S+");
 
-	auto get_splite_items = matchAll(in_buff, r);
-	assert(get_splite_items, "Could not splite by space!");
+	auto getSpliteItems = matchAll(inBuff, r);
+	assert(getSpliteItems, "Could not splite by space!");
 	string output[];
-	foreach(s; get_splite_items)
+	foreach(s; getSpliteItems)
 	    output ~= cast(string) s.hit;
 	return output;
     }
-    void parse_middleware(string to_parse, out string[] before_middleware, out string[] after_middleware)
+    void parseMiddleware(string toParse, out string[] beforeMiddleware, out string[] afterMiddleware)
     {
-	int before_pos, after_pos, semicolon_pos;
-	before_pos = to_parse.indexOf(_before_flag);
-	after_pos = to_parse.indexOf(_after_flag);
-	semicolon_pos = to_parse.indexOf(";");
-	assert(before_pos <= after_pos, "after position and before position worry");
-	//assert(before_pos 
-	writeln("to_parse: ", to_parse, " before_pos: ",before_pos);
-	if(before_pos<0)
-	    before_pos = to_parse.length-1;
+	int beforePos, afterPos, semicolonPos;
+	beforePos = toParse.indexOf(_beforeFlag);
+	afterPos = toParse.indexOf(_afterFlag);
+	semicolonPos = toParse.indexOf(";");
+	assert(beforePos <= afterPos, "after position and before position worry");
+	//assert(beforePos 
+	writeln("toParse: ", toParse, " beforePos: ",beforePos);
+	if(beforePos<0)
+	    beforePos = toParse.length-1;
 	else
-	    before_pos += _before_flag.length;
-	if(after_pos<0)
-	    after_pos = to_parse.length-1;
+	    beforePos += _beforeFlag.length;
+	if(afterPos<0)
+	    afterPos = toParse.length-1;
 	else
-	    after_pos += _after_flag.length;
-	if(semicolon_pos<0)
-	    semicolon_pos = to_parse.length-1;
-	if(before_pos < semicolon_pos)
-	    before_middleware = split(to_parse[before_pos..semicolon_pos], ',');
-	after_middleware = split(to_parse[after_pos..$], ',');
-	writeln("before_middleware: ", before_middleware, " after_middleware: ", after_middleware);
+	    afterPos += _afterFlag.length;
+	if(semicolonPos<0)
+	    semicolonPos = toParse.length-1;
+	if(beforePos < semicolonPos)
+	    beforeMiddleware = split(toParse[beforePos..semicolonPos], ',');
+	afterMiddleware = split(toParse[afterPos..$], ',');
+	writeln("beforeMiddleware: ", beforeMiddleware, " afterMiddleware: ", afterMiddleware);
     }
     
 private:
-    string _controller_prefix="Controller";
-    string _before_flag= "before:";
-    string _after_flag= "after:";
+    string _controllerPrefix="Controller";
+    string _beforeFlag= "before:";
+    string _afterFlag= "after:";
 }
 
 unittest
 {
     ConfigParse new_parse = new ConfigParse("router.conf");
-    //new_parse.set_file_path("router.conf");
-    RouterContext[] test_router_context = new_parse.do_parse();
+    //new_parse.setFilePath("router.conf");
+    RouterContext[] test_router_context = new_parse.doParse();
     foreach(item; test_router_context)
     {
 	writeln("method: ",item.method, " path: ", item.path, " hander: ", item.hander, " middleWareAfter: ", item.middleWareAfter, " middleWareBefore: ", item.middleWareBefore);
