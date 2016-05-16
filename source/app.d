98b13992659d23895e1f4efac5fd8269cbc03d42
渡世白玉 <dushibaiyu@yahoo.com>
2016-05-16 18:43:31 +0800

    middleware example

--- source/app.d
+++ source/app.d
@@ -1,24 +1,26 @@
 import std.stdio;
 import std.functional;
+import std.experimental.logger;
 
 import hunt.http.cookie;
 
 import hunt.webapplication;
+import application.middleware;
 
 void hello(Request, Response res)
 {
     res.setContext("hello world");
     res.setHeader("content-type","text/html;charset=UTF-8");
-    res.done();
 }
 
 void main()
 {
+    globalLogLevel(LogLevel.error);
     WebApplication app = new WebApplication();
     app.setRouterConfig(new ConfigParse("config/router.conf"));
-    app.addRouter("GET","/test",toDelegate(&hello));
-    app.addRouter("GET","/ttt",toDelegate(&hello));
-    app.bind(8080);
+    app.addRouter("GET","/test",toDelegate(&hello)).addRouter("GET","/ttt",toDelegate(&hello));
+    app.setGlobalAfterPipelineFactory(new GAMFactory).setGlobalBeforePipelineFactory(new GBMFactory);
+    app.group(new EventLoopGroup()).bind(8080);
     app.run();
 }
 
--- source/application/controllers/index.d
+++ source/application/controllers/index.d
@@ -1,8 +1,6 @@
 module application.controllers.index;
 
-import hunt.http.controller;
-import hunt.http.request;
-import hunt.http.response;
+import hunt.http;
 
 class IndexController : IController
 {
@@ -11,9 +9,8 @@ class IndexController : IController
     void show(Request req, Response res)
     {
         import std.stdio;
-        writeln("do show!");
-        res.setContext("hello world");
+       // writeln("do show!");
+        res.setContext("hello world<br/>");
         res.setHeader("content-type","text/html;charset=UTF-8");
-        res.done();
     }
 }
