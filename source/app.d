8302905b19f73604a54109486a0d74bf4ba468c0
渡世白玉 <dushibaiyu@yahoo.com>
2016-05-17 11:44:55 +0800

    test

--- source/app.d
+++ source/app.d
@@ -11,6 +11,7 @@ void hello(Request, Response res)
 {
     res.setContext("hello world");
     res.setHeader("content-type","text/html;charset=UTF-8");
+    res.done();
 }
 
 void main()
@@ -19,8 +20,8 @@ void main()
     WebApplication app = new WebApplication();
     app.setRouterConfig(new ConfigParse("config/router.conf"));
     app.addRouter("GET","/test",toDelegate(&hello)).addRouter("GET","/ttt",toDelegate(&hello));
-    app.setGlobalAfterPipelineFactory(new GAMFactory).setGlobalBeforePipelineFactory(new GBMFactory);
-    app.group(new EventLoopGroup()).bind(8080);
+   // app.setGlobalAfterPipelineFactory(new GAMFactory).setGlobalBeforePipelineFactory(new GBMFactory);
+    app.group(new EventLoopGroup()).bind(8086);
     app.run();
 }
 
