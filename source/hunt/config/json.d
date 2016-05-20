b9767c2423da74366c79db130921ae9269391bb2
donglei@donglei.com <donglei@donglei.com>
2016-05-20 11:06:26 +0800

    批量加文件注释

--- source/hunt/config/json.d
+++ source/hunt/config/json.d
@@ -1,1 +1,11 @@
+/*
+ * Hunt - a framework for web and console application based on Collie using Dlang development
+ *
+ * Copyright (C) 2015-2016  Shanghai Putao Technology Co., Ltd 
+ *
+ * Developer: putao's Dlang team
+ *
+ * Licensed under the BSD License.
+ *
+ */
 module hunt.config.json;
