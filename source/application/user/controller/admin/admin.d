239d8bc0823512673726e85ee6ff722d1cd7b25d
jiyahan <jiyh@putao.com>
2016-05-20 11:36:16 +0800

    Add some tests

---
+++ source/application/user/controller/admin/admin.d
@@ -0,0 +1,15 @@
+module application.user.controller.admin.admin;
+
+import hunt.web.http;
+
+class AdminController : IController
+{
+    mixin HuntDynamicCallFun;
+    
+    void show(Request req, Response res)
+    {
+        res.setContext("<h2>"~"line:"~__LINE__~",function:"~__FUNCTION__);
+        res.setHeader("content-type","text/html;charset=UTF-8");
+	res.done();
+    }
+}
