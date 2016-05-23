c423eccf131fc7f24e2622a8f3fafa63e09b818c
渡世白玉 <dushibaiyu@yahoo.com>
2016-05-23 16:27:52 +0800

    rebuild the config

--- source/app.d
+++ source/app.d
@@ -41,12 +41,13 @@ void show()
 void main()
 {
     writeln("hello world");
-    globalLogLevel(LogLevel.error);
+   // globalLogLevel(LogLevel.error);
     EventLoop loop = new EventLoop();
     
     WebApplication app = new WebApplication(loop);
 
     app.setRouterConfig(new ConfigMultipleModule("config/router.conf", "config/application.conf"));
+   //app.setRouterConfig(new ConfigSignalModule("config/router.api.conf"));
     //app.addRouter("GET","/test",toDelegate(&hello)).addRouter("GET","/ttt",toDelegate(&hello));
     //app.setGlobalAfterPipelineFactory(new GAMFactory).setGlobalBeforePipelineFactory(new GBMFactory);
 //    app.group(new EventLoopGroup(1));
