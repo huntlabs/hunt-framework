90d2166bcbd25ca776bbb47dba9d5ce3527fe4ae
jiyahan <jiyh@putao.com>
2016-05-23 14:23:55 +0800

    split routerconfig.d

--- source/app.d
+++ source/app.d
@@ -42,7 +42,7 @@ void main()
     //globalLogLevel(LogLevel.error);
     WebApplication app = new WebApplication();
 
-    app.setRouterConfig(new ConfigParse("config/router.conf", "config/application.conf"));
+    app.setRouterConfig(new ConfigMultipleModule("config/router.conf", "config/application.conf"));
     //app.addRouter("GET","/test",toDelegate(&hello)).addRouter("GET","/ttt",toDelegate(&hello));
     //app.setGlobalAfterPipelineFactory(new GAMFactory).setGlobalBeforePipelineFactory(new GBMFactory);
     app.group(new EventLoopGroup(1)).bind(8080);
