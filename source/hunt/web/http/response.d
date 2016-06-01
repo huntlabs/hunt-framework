92c5aab5f4f6768e27553795351c492c3b90f112
渡世白玉 <dushibaiyu@yahoo.com>
2016-06-01 16:32:37 +0800

    add redirect

--- source/hunt/web/http/response.d
+++ source/hunt/web/http/response.d
@@ -52,12 +52,12 @@ class Response
         _rep.Header.statusCode(code);
     }
 
-	///return json value
-	void writeJson(JSONValue json)
-	{
-		setHeader("Content-Type", "application/json;charset=UTF-8");
-		setContext(json.toString());
-	}
+    ///return json value
+    void writeJson(JSONValue json)
+    {
+            setHeader("Content-Type", "application/json;charset=UTF-8");
+            setContext(json.toString());
+    }
     /**
 	* 设置Session Cookie
 	*/
@@ -75,6 +75,13 @@ class Response
     @property httpResponse()
     {
         return _rep;
+    }
+    
+    void redirect(string url, bool is301 = false)
+    {
+        httpResponse.Header.statusCode((is301 ? 301 : 202));
+        httpResponse.Header.setHeaderValue("Location",url);
+        httpResponse.done();
     }
 
 private:
