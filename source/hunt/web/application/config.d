d7ebc6dffa8e72aa54d78697810d5af95e5c0c68
dushibaiyu <dushibaiyu@yahoo.com>
2016-06-14 14:52:55 +0800

    web app config update

--- source/hunt/web/application/config.d
+++ source/hunt/web/application/config.d
@@ -75,29 +75,85 @@ class IniWebConfig : WebConfig
         }
         else
         {
-            auto ip = value;
-            value = _ini.get("server.isipv6");
-            if(value.length == 0)
-            {
-                return new InternetAddress(ip,port);
-            }
-            
-            bool ip6 = to!bool(value);
+            bool ip6 = _ini.get!bool("server.isipv6", false);
             if(ip6)
-                return new Internet6Address(ip,port);
+                return new Internet6Address(value,port);
             else
-                return new InternetAddress(ip,port);
+                return new InternetAddress(value,port);
         }
     }
     
     override uint threadSize()
     {
-        auto value = _ini.get("server.threadsize");
-        if(value.length == 0)
-            return super.threadSize();
-        return to!uint(value);
+        return _ini.get!uint("server.threadsize",totalCPUs);
     }
     
+    
+    override WebSocketFactory webSocketFactory()
+    {
+        auto value = _ini.get("server.websocket");
+        WebSocketFactory socket = null;
+        if(value.length)
+        {
+            auto obj = Object.factory(value);
+            if(obj) socket = cast(WebSocketFactory)obj;
+        }
+        return socket;
+    }
+    
+    override uint keepAliveTimeOut()
+    {
+        return _ini.get!uint("server.timeout",30);
+    }
+    
+    override uint httpMaxBodySize(){return _ini.get!uint("http.maxbody",8 * 1024 * 1024);}
+    
+    override uint httpMaxHeaderSize(){return _ini.get!uint("http.maxheader",4 * 1024);}
+    
+    override uint httpHeaderStectionSize(){return _ini.get!uint("http.headersection",2048);}
+    
+    override uint httpRequestBodyStectionSize(){return _ini.get!uint("http.requestsection", 16 * 1024);}
+    
+    override uint httpResponseBodyStectionSize(){return _ini.get!uint("http.responsesection", 16 * 1024);}
+    
+    override ServerSSLConfig sslConfig()
+    {
+        import std.string;
+        auto mode = _ini.get("server.ssl.mode");
+        if(mode.length == 0) return null;
+        SSLMode md;
+        mode = toUpper(mode);
+        switch(mode)
+        {
+            case "SSLV2V3" :
+                md = SSLMode.SSLv2v3;
+                break;
+            case "TLSV1" :
+                md = SSLMode.TLSv1;
+                break;
+            case "TLSV1_1" :
+                md = SSLMode.TLSv1_1;
+                break;
+            case "TLSV1_2" :
+                md = SSLMode.TLSv1_2;
+                break;
+            default:
+                throw new Exception("ssl Mode is not support!");
+        }
+        ServerSSLConfig ssl = new ServerSSLConfig(md);
+        ssl.certificateFile(_ini.get("server.ssl.certificate"));
+        ssl.privateKeyFile(_ini.get("server.ssl.privatekey"));
+     //   ssl.cipherList(_ini.get("server.ssl.cipher"));
+        
+        return ssl;
+    }
+    
+    override RouterConfigBase routerConfig()
+    {
+        auto value = _ini.get("router.configpath");
+        import hunt.web.router.configsignalmodule;
+        return new ConfigSignalModule(value);
+    }
     
 private:
     Conf _ini;
