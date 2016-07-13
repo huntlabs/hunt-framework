c0734390dcf3a6feb243bb7e298165e52af14eb1
donglei <donglei@donglei.com>
2016-07-13 11:44:19 +0800

    xx

--- source/hunt/http/response.d
+++ source/hunt/http/response.d
@@ -33,105 +33,117 @@ class Response
     auto setHeader(T = string)(string key, T value)
     {
         _rep.Header.setHeaderValue(key, value);
-		return this;
+        return this;
     }
 
     auto write(ubyte[] data)
     {
         _rep.Body.write(data);
-		return this;
+        return this;
     }
 
     auto setContext(string str)
     {
         _rep.Body.write(cast(ubyte[]) str);
-		return this;
+        return this;
     }
 
     auto setContext(ubyte[] data)
     {
         _rep.Body.write(data);
-		return this;
+        return this;
     }
 
-	///set http status code eg. 404 200
+    ///set http status code eg. 404 200
     auto setHttpStatusCode(int code)
     {
         _rep.Header.statusCode(code);
-		return this;
+        return this;
     }
 
     ///return json value
-    auto writeJson(JSONValue json)
+    auto json(JSONValue json)
     {
-        setHeader("Content-Type", "application/json;charset=UTF-8");
-        setContext(json.toString());
-		return this;
+        setContext(json.toString()).setHeader("Content-Type", "application/json;charset=UTF-8");
+        return this;
     }
+    ///render json string value
+    auto json(string jsonString)
+    {
+        setContext(jsonString).setHeader("Content-Type", "application/json;charset=UTF-8");
+        return this;
+    }
+    ///render html string 
+    auto html(string htmlString, string content_type = "text/html;charset=UTF-8")
+    {
+        setContext(htmlString).setHeader("content-type", content_type);
+        return this;
+    }
+    ///render plain text string 
+    auto plain(string textString, string content_type = "text/plain;charset=UTF-8")
+    {
+        setContext(textString).setHeader("content-type", content_type);
+        return this;
+    }
+
     /**
 	* 设置Session Cookie
 	*/
     auto setCookie(string name, string value, int expires, string path = "/", string domain = null)
     {
         import std.typecons;
-        auto cookie = scoped!Cookie(name, value, ["path" : path, "domain" : domain,
-            "expires" : printDate(cast(DateTime) Clock.currTime(UTC()) + dur!"seconds"(expires))]); //栈中优化
-        ///TODO set into base
+
+        auto cookie = scoped!Cookie(name, value, ["path" : path, "domain"
+                : domain, "expires"
+                : printDate(cast(DateTime) Clock.currTime(UTC()) + dur!"seconds"(expires))]); //栈中优化
         this.setHeader("set-cookie", cookie.output(""));
-		return this;
+        return this;
     }
 
+    pragma(inline, true) final @property Header()
+    {
+        return _rep.Header();
+    }
 
-	pragma(inline,true)
-	final @property Header()
-	{
-		return _rep.Header();
-	}
-	
-	pragma(inline,true)
-	final @property Body()
-	{
-		return _rep.Body();
-	}
-	
-	final bool append(ubyte[] data)
-	{
-		return _rep.append(data);
-	}
-	
-	pragma(inline)
-	final bool done()
-	{
-		setHeader("X-Powered-By",XPoweredBy);
-		return _rep.done(null, 0);
-	}
-	
-	pragma(inline)
-	final bool done(string file)
-	{
-		setHeader("X-Powered-By",XPoweredBy);
-		return _rep.done(file, 0);
-	}
+    pragma(inline, true) final @property Body()
+    {
+        return _rep.Body();
+    }
 
-	pragma(inline)
-	final bool done(string file, ulong begin)
-	{
-		setHeader("X-Powered-By",XPoweredBy);
-		return _rep.done(file, begin);
-	}
-	
-	pragma(inline)
-	final void close()
-	{
-		_rep.close();
-	}
-    
+    final bool append(ubyte[] data)
+    {
+        return _rep.append(data);
+    }
+
+    pragma(inline) final bool done()
+    {
+        setHeader("X-Powered-By", XPoweredBy);
+        return _rep.done(null, 0);
+    }
+
+    pragma(inline) final bool done(string file)
+    {
+        setHeader("X-Powered-By", XPoweredBy);
+        return _rep.done(file, 0);
+    }
+
+    pragma(inline) final bool done(string file, ulong begin)
+    {
+        setHeader("X-Powered-By", XPoweredBy);
+        return _rep.done(file, begin);
+    }
+
+    pragma(inline) final void close()
+    {
+        _rep.close();
+    }
+
     void redirect(string url, bool is301 = false)
     {
-        
-		_rep.Header.statusCode((is301 ? 301 : 302));
-		_rep.Header.setHeaderValue("Location",url);
-		_rep.done();
+
+        _rep.Header.statusCode((is301 ? 301 : 302));
+        _rep.Header.setHeaderValue("Location", url);
+        _rep.done();
     }
 
 private:
