4cf433b6c25835648e95cf8fc9dc5792c0dbf0f6
Brian Zou <zoujiaqing@gmail.com>
2016-07-18 15:18:58 +0800

    Update versions.d

--- source/hunt/versions.d
+++ source/hunt/versions.d
@@ -1,4 +1,14 @@
-﻿module hunt.versions;
+/*
+ * Hunt - a framework for web and console application based on Collie using Dlang development
+ *
+ * Copyright (C) 2015-2016  Shanghai Putao Technology Co., Ltd
+ *
+ * Developer: putao's Dlang team
+ *
+ * Licensed under the BSD License.
+ *
+ */
+
+module hunt.versions;
 
 enum HUNT_VERSION = "";
-
