137215cd082520cde08af09f9264841db8634f73
dushibaiyu <dushibaiyu@yahoo.com>
2016-07-18 17:49:06 +0800

    add model class

--- source/hunt/application/model.d
+++ source/hunt/application/model.d
@@ -2,6 +2,83 @@
 
 import entity;
 
+abstract class Model(T) if(is(T == class) || is(T == struct))
+{
+	alias MQuery = Query!T;
+	alias Iterator = MQuery.Iterator;
+
+	this()
+	{}
+
+	this (DataBase db)
+	{
+		_dbc = db;
+	}
+
+	final @property MQuery query()
+	{
+		if(_queryc is null)
+			_queryc = new MQuery(database);
+		return _queryc;
+	}
+
+	final @property DataBase database()
+	{
+		if(_dbc is null)
+			_dbc = DB;
+		return _dbc;
+	}
+
+	final Iterator Select(string table = "")()
+	{
+		return query.Select!(table)();
+	}
+	
+	final Iterator Select(string sql)
+	{
+		return query.Select(sql);
+	}
+	
+	final void Insert(string table = "")(ref T v)
+	{
+		query.Insert!(table)(v);
+	}
+	
+	final void Update(string table = "")(ref T v)
+	{
+		query.Update!(table)(v);
+	}
+	
+	final void Update(string table = "")(ref T v, string where)
+	{
+		query.Update!(table)(v,where);
+	}
+	
+	final void Update(string table = "")(ref T v, WhereBuilder where)
+	{
+		query.Update!(table)(v,where);
+	}
+	
+	final void Delete(string table = "")(ref T v)
+	{
+		query.Delete!(table)(v,where);
+	}
+	
+	final void Delete(string table = "")(ref T v, string where)
+	{
+		query.Delete!(table)(v,where);
+	}
+	
+	final void Delete(string table = "")(ref T v, WhereBuilder where)
+	{
+		query.Delete!(table)(v,where);
+	}
+
+private:
+	DataBase _dbc;
+	MQuery _queryc;
+}
+
 @property DataBase DB()
 {
 	if(_db is null)
