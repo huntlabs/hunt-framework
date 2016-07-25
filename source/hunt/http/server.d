bfdc3fa242dfc9300ad268d2075da5b5b7a9817a
nilufanier <449205881@qq.com>
2016-07-25 10:37:04 +0800

    debug windows crash
    debug windows crash

--- source/hunt/http/server.d
+++ source/hunt/http/server.d
@@ -68,8 +68,13 @@ final class HTTPServer
     */
     auto setSSLConfig(ServerSSLConfig config) 
     {
-        _server.setSSLConfig(config);
-        return this;
+    	version (Windows)
+    	{}
+    	else
+    	{
+        	_server.setSSLConfig(config);
+        	return this;
+        }
     } 
     
     /**
