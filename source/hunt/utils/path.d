0707eec7752e791e8981213466ab751ceb3c6fdd
donglei <donglei@donglei.com>
2016-08-26 11:17:12 +0800

    add theConfigPath

--- source/hunt/utils/path.d
+++ source/hunt/utils/path.d
@@ -4,19 +4,32 @@ module hunt.utils.path;
 public import std.path;
 
 //TODO: if in the bin path, the path will be start path
+//TODO: if in the bin path, the path will be start path
 @property theExecutorPath()
 {
-    return _theExecutorPath;
+	return _theExecutorPath;
 }
 
-private:
+@property theConfigPath()
+{
+	return _theConfigPath;
+}
 
-shared string _theExecutorPath = "";
+private shared string _theExecutorPath = "";
+private shared string _theConfigPath = "";
 
 shared static this()
 {
-    import core.runtime;
-
-    auto list = Runtime.args();
-    _theExecutorPath = absolutePath(dirName(list[0]));
+	import core.runtime;
+	
+	auto list = Runtime.args();
+	_theExecutorPath = absolutePath(dirName(list[0]));
+	if(list.length == 1)
+	{
+		_theConfigPath = _theExecutorPath;
+	}
+	else
+	{
+		_theConfigPath = absolutePath(list[1]);
+	}
 }
