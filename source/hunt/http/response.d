cc854be35ad5ed315184a81f1acd13939c547408
zoujiaqing <zoujiaqing@debian.mixos.org>
2016-12-30 18:47:53 +0800

    Adjust code style

--- source/hunt/http/response.d
+++ source/hunt/http/response.d
@@ -8,6 +8,7 @@
  * Licensed under the BSD License.
  *
  */
+
 module hunt.http.response;
 
 import std.datetime;
@@ -20,118 +21,136 @@ import collie.codec.http.httpmessage;
 
 import hunt.http.cookie;
 import hunt.utils.string;
-
 import hunt.versions;
 
 enum XPoweredBy = "Hunt " ~ HUNT_VERSION;
 
 final class Response : ResponseBuilder
 {
-    this(ResponseHandler resp)
-    {
+	this(ResponseHandler resp)
+	{
 		super(resp);
 		setHttpStatusCode(200);
-    }
+	}
 
-    auto setHeader(T = string)(string key, T value)
-    {
+	auto setHeader(T = string)(string key, T value)
+	{
 		header!T(key,value);
-        return this;
-    }
+
+		return this;
+	}
 
 	auto setHeader(T = string)(HTTPHeaderCode key, T value)
 	{
 		header!T(key,value);
+
 		return this;
 	}
 
-    auto setContext(string str)
-    {
+	auto setContext(string str)
+	{
 		setBody(cast(ubyte[]) str);
-        return this;
-    }
 
-    auto setContext(ubyte[] data)
-    {
+		return this;
+	}
+
+	auto setContext(ubyte[] data)
+	{
 		setBody(data);
-        return this;
-    }
 
-    ///set http status code eg. 404 200
-    auto setHttpStatusCode(ushort code)
-    {
+		return this;
+	}
+
+	///set http status code eg. 404 200
+	auto setHttpStatusCode(ushort code)
+	{
 		status(code,HTTPMessage.statusText(code));
-        return this;
-    }
 
-    ///return json value
-    auto json(JSONValue js)
-    {
+		return this;
+	}
+
+	///return json value
+	auto json(JSONValue js)
+	{
 		json(js.toString());
-        return this;
-    }
-    ///render json string value
-    auto json(string jsonString)
-    {
-		setHeader(HTTPHeaderCode.CONTENT_TYPE, "application/json;charset=UTF-8").setContext(jsonString);
-        return this;
-    }
-    ///render html string 
-    auto html(string htmlString, string content_type = "text/html;charset=UTF-8")
-    {
+
+		return this;
+	}
+	///render json string value
+	auto json(string jsonString)
+	{
+		setHeader(HTTPHeaderCode.CONTENT_TYPE, "application/json;charset=utf-8").setContext(jsonString);
+
+		return this;
+	}
+	///render html string 
+	auto html(string htmlString, string content_type = "text/html;charset=utf-8")
+	{
 		setHeader(HTTPHeaderCode.CONTENT_TYPE, content_type).setContext(htmlString);
-        return this;
-    }
-    ///render plain text string 
-    auto plain(string textString, string content_type = "text/plain;charset=UTF-8")
-    {
+
+		return this;
+	}
+	///render plain text string 
+	auto plain(string textString, string content_type = "text/plain;charset=utf-8")
+	{
 		setHeader(HTTPHeaderCode.CONTENT_TYPE, content_type).setContext(textString);
-        return this;
-    }
 
-    /**
-	* 设置Session Cookie
-	*/
-    auto setCookie(string name, string value, int expires, string path = "/", string domain = null)
-    {
-        import std.typecons;
-        auto cookie = scoped!Cookie(name, value, ["path" : path, "domain"
-                : domain, "expires"
-                : printDate(cast(DateTime) Clock.currTime(UTC()) + dur!"seconds"(expires))]); //栈中优化
-        setHeader(HTTPHeaderCode.SET_COOKIE, cookie.output(""));
-        return this;
-    }
+		return this;
+	}
 
-    pragma(inline) final void done()
-    {
+	/**
+	 * set Cookie
+	 */
+	auto setCookie(string name, string value, int expires, string path = "/", string domain = null)
+	{
+		import std.typecons;
+		auto cookie = scoped!Cookie(name, value, ["path" : path, "domain"
+				: domain, "expires"
+				: printDate(cast(DateTime) Clock.currTime(UTC()) + dur!"seconds"(expires))]); //栈中优化
+
+		setHeader(HTTPHeaderCode.SET_COOKIE, cookie.output(""));
+
+		return this;
+	}
+
+	pragma(inline) final void done()
+	{
 		if(_isDone) return;
+
 		_isDone = true;
 		setHeader(HTTPHeaderCode.X_POWERED_BY, XPoweredBy);
-        sendWithEOM();
-    }
+		sendWithEOM();
+	}
 
-    void redirect(string url, bool is301 = false)
-    {
+	void redirect(string url, bool is301 = false)
+	{
 		if(_isDone) return;
+
 		setHttpStatusCode((is301 ? 301 : 302));
 		setHeader(HTTPHeaderCode.LOCATION, url);
 		connectionClose();
+
 		done();
-    }
+	}
 
 	void do404(string body_ = "",string contentype = "text/plain;charset=UTF-8")
 	{
 		if(_isDone) return;
+
 		setHttpStatusCode(404);
 		header(HTTPHeaderCode.CONTENT_TYPE,"text/plain;charset=UTF-8");
+
 		if(body_.length > 0)
 			setContext(body_);
+
 		connectionClose();
+
 		done();
 	}
 
 package(hunt.http):
-	void clear(){
+	void clear()
+	{
 		setResponseHandler(null);
 	}
 private:
