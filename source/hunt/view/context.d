8dabcd39bb7bc827e434d4d2bca487429d3022d1
viile <dakgzhu@foxmail.com>
2017-02-21 20:35:11 +0800

    fixed view context

--- source/hunt/view/context.d
+++ source/hunt/view/context.d
@@ -12,6 +12,12 @@ class ViewContext
 {
 	public Variant[string] vars;
 
+	void opIndexAssign(T)(string name, T val)
+	{
+		if (name !in vars)
+			vars[name] = Variant();
+		vars[name] = val;
+	}
 	void opDispatch(string name, T)(T val)
 	{
 		if (name !in vars)
