896b2cd3291d9c65fce9ce9565117806c10a134e
donglei <donglei@donglei.com>
2017-03-17 18:59:40 +0800

    can run

---
+++ examples/orm/source/app/dao/user.d
@@ -0,0 +1,18 @@
+﻿module app.dao.user;
+import app.entity.user;
+
+import hunt.orm.entity;
+import entity;
+
+class UserDao
+{
+	static void registerUser()
+	{
+		EntityManager manager = entityManagerFactory.createEntityManager();
+		scope(exit){manager.close();}
+		User user = new User();
+		user.name = "donglei";
+		manager.save(user);
+	}
+}
+
