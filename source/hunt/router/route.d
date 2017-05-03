2be1e8947b85236782757192179764cf4f67fd1b
shove <shove@DESKTOP-M6I4V7R>
2017-05-03 17:03:38 +0800

    Add static file render features, (html, js, css, jpg... )

---
+++ source/hunt/router/route.d
@@ -0,0 +1,197 @@
+﻿/*
+ * Hunt - a framework for web and console application based on Collie using Dlang development
+ *
+ * Copyright (C) 2015-2017  Shanghai Putao Technology Co., Ltd
+ *
+ * Developer: HuntLabs
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
+
+module hunt.router.route;
+
+import hunt.router.define;
+import hunt.application.controller;
+
+class Route
+{
+    this()
+    {
+        // Constructor code
+    }
+
+    public
+    {
+        void setGroup(string groupValue)
+        {
+            this._group = groupValue;
+        }
+
+        string getGroup()
+        {
+            return this._group;
+        }
+
+        void setUrlTemplate(string urlTemplate)
+        {
+            this._urlTemplate = urlTemplate;
+        }
+
+        string getUrlTemplate()
+        {
+            return this._urlTemplate;
+        }
+
+        void setRoute(string routeValue)
+        {
+            this._route = routeValue;
+        }
+
+        string getRoute()
+        {
+            return this._route;
+        }
+
+        void setParamKeys(string[int] paramKeys)
+        {
+            this._paramKeys = paramKeys;
+        }
+
+        string[int] getParamKeys()
+        {
+            return this._paramKeys;
+        }
+
+        void setParams(string[string] params)
+        {
+            this._params = params;
+        }
+
+        string[string] getParams()
+        {
+            return this._params;
+        }
+
+        void setPattern(string patternValue)
+        {
+            this._pattern = patternValue;
+        }
+
+        string getPattern()
+        {
+            return this._pattern;
+        }
+
+        void setRegular(bool regularValue)
+        {
+            this._regular = regularValue;
+        }
+
+        bool getRegular()
+        {
+            return this._regular;
+        }
+
+        void setModule(string moduleValue)
+        {
+            this._module = moduleValue;
+        }
+
+        string getModule()
+        {
+            return this._module;
+        }
+
+        void setController(string controllerValue)
+        {
+            this._controller = controllerValue;
+        }
+
+        string getController()
+        {
+            return this._controller;
+        }
+
+        void setAction(string actionValue)
+        {
+            this._action = actionValue;
+        }
+
+        string getAction()
+        {
+            return this._action;
+        }
+
+        void setMethods(HTTP_METHOS[] methods)
+        {
+            this._methods = methods;
+        }
+
+        HTTP_METHOS[] getMethods()
+        {
+            return this._methods;
+        }
+
+        @property handle()
+        {
+            return this._handle;
+        }
+
+        @property void handle(HandleFunction handle)
+        {
+            this._handle = handle;
+        }
+        
+        @property staticFilePath()
+        {
+        	return this._staticFilePath;
+        }
+        
+        @property void staticFilePath(string path)
+        {
+        	this._staticFilePath = path;
+        }
+    }
+
+    private
+    {
+        // Route group name
+        string _group;
+
+        // Regex template
+        string _urlTemplate;
+
+        // http uri params
+        string[int] _paramKeys;
+
+        string[string] _params;
+
+        // like uri path
+        string _pattern;
+
+        // path to module.controller.action
+        string _route;
+
+        // use regex?
+        bool _regular;
+
+        // hunt module
+        string _module;
+
+        // hunt controller
+        string _controller;
+
+        // hunt action
+        string _action;
+
+        // handle function
+        HandleFunction _handle;
+
+        // allowd http methods
+        HTTP_METHOS[] _methods = [ HTTP_METHOS.GET, HTTP_METHOS.POST, HTTP_METHOS.PUT, HTTP_METHOS.DELETE ];
+        
+        // staticDir:path
+        string _staticFilePath;
+    }
+}
---
+++ source/hunt/router/routegroup.d
@@ -0,0 +1,123 @@
+﻿/*
+ * Hunt - a framework for web and console application based on Collie using Dlang development
+ *
+ * Copyright (C) 2015-2017  Shanghai Putao Technology Co., Ltd
+ *
+ * Developer: HuntLabs
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
+
+module hunt.router.routegroup;
+
+import hunt.router.define;
+import hunt.router.route;
+
+class RouteGroup
+{
+    this(string name = DEFAULT_ROUTE_GROUP)
+    {
+        this._name = name;
+    }
+
+    public
+    {
+        void setName(string name)
+        {
+            this._name = name;
+        }
+
+        string getName()
+        {
+            return this._name;
+        }
+
+        RouteGroup addRoute(Route route)
+        {
+            if (route.getRegular())
+            {
+                this._regexRoutes ~= route;
+            }
+            else
+            {
+                this._routes[route.getPattern()] = route;
+            }
+
+            string mca = ((route.getModule()) ? route.getModule() ~ "." : "") ~ route.getController() ~ "." ~ route.getAction();
+
+            this._mcaRoutes[mca] = route;
+
+            return this;
+        }
+
+        Route getRoute(string mca)
+        {
+            return this._mcaRoutes.get(mca, null);
+        }
+
+        Route match(string path)
+        {
+            // TODO: 引用类型使用结构体二次包装
+            Route route;
+
+            tracef("Match in group: %s, %s", this._name, this._routes);
+
+            route = this._routes.get(path, null);
+
+            if (route)
+            {
+                return route;
+            }
+            else
+            {
+            	foreach(key, value; this._routes)
+            	{
+            		import std.string;
+            		if (path.startsWith(key))
+            		{
+            			route = this._routes.get(key, null);
+            			
+            			if (route.staticFilePath != string.init)
+            			{
+            				return route;
+            			}
+            		}
+            	}
+            }
+
+            import std.regex;
+            
+            foreach (r; this._regexRoutes)
+            {
+                auto matched = path.match(regex(r.getPattern()));
+
+                if (matched)
+                {
+                    route = r;
+
+                    string[string] params;
+
+                    foreach(i, key; route.getParamKeys())
+                    {
+                        params[key] = matched.captures[i + 1];
+                    }
+
+                    route.setParams(params);
+
+                    return route;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    private
+    {
+        string _name;
+        Route[string] _routes;
+        Route[string] _mcaRoutes;
+        Route[] _regexRoutes;
+    }
+}
