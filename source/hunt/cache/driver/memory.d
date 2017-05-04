4de51f3c66e852f7f88c1bb1f26a4efc9c12a37b
zoujiaqing <zoujiaqing@gmail.com>
2017-05-04 18:37:24 +0800

    cache module

--- source/hunt/cache/driver/memory.d
+++ source/hunt/cache/driver/memory.d
@@ -1,14 +1,45 @@
-module hunt.cache.memory;
+module hunt.cache.driver.memory;
 
-import hunt.cache;
+import hunt.cache.driver.base;
 
-class Memory : Store
+import core.memory;
+import core.sync.rwmutex;
+
+class MemoryBuffer
+{
+	string key;
+	ulong length;
+	ubyte[] *ptr;
+	MemoryBuffer prv;
+	MemoryBuffer next;
+
+	this(string key,ubyte[] value,ulong length)
+	{
+		this.key = key;
+		this.length = length;
+		ptr = cast(ubyte[] *)GC.malloc(length);
+		*ptr = value;
+	}
+
+	void clear()
+	{
+		length = 0;
+		GC.free(ptr);
+	}
+
+	~this()
+	{
+		if(length)GC.free(ptr);
+	}
+}
+
+class MemoryCache : AbstractCache
 {
 	private ulong trunkNum;
 	private ulong trunkSize;
-	private BufferTrunk[string] map;
-	private BufferTrunk head;
-	private BufferTrunk tail;
+	private MemoryBuffer[string] map;
+	private MemoryBuffer head;
+	private MemoryBuffer tail;
 	private ReadWriteMutex _mutex;
 
 	this()
@@ -20,24 +51,31 @@ class Memory : Store
 	{
 		_mutex.destroy();
 	}
+
 	ulong getTrunkNum()
 	{
 		return trunkNum;
 	}
+
 	ulong getTrunkSize()
 	{
 		return trunkSize;
 	}
-	override bool set(string key,ubyte[] value)
+
+	override bool set(string key, ubyte[] value, int exprie = 0)
 	{
 		if(isset(key))return false;
+
 		_mutex.writer.lock();
 		scope(exit) _mutex.writer.unlock();
-		BufferTrunk trunk = new BufferTrunk(key,value,value.length);
+
+		auto trunk = new MemoryBuffer(key,value,value.length);
+
 		map[key] = trunk;
 		trunkNum++;
 		trunkSize+=value.length;
-		if(head is BufferTrunk.init){
+        
+		if(head is MemoryBuffer.init){
 			head = trunk;
 			tail = trunk;
 		}else{
@@ -45,53 +83,69 @@ class Memory : Store
 			trunk.prv = tail;
 			tail = trunk;
 		}
+
 		return true;
 	}
-	override ubyte[]  get(string key)
+
+	override ubyte[] get(string key)
 	{
 		_mutex.reader.lock();
 		scope(exit) _mutex.reader.unlock();
+
 		return map.get(key,null) ? *(map[key].ptr) : null;
 	}
+    
 	override bool isset(string key)
 	{
 		if(map.get(key,null) is null) 
 			return false;
+
 		return true;
 	}
-	override bool remove(string key)
+
+	override bool erase(string key)
 	{
 		_mutex.writer.lock();
 		scope(exit) _mutex.writer.unlock();
+
 		if(!isset(key))return true;
+
 		if(map[key].prv)map[key].prv.next = map[key].next;
 		if(map[key].next)map[key].next.prv = map[key].prv;
+
 		trunkNum--;
 		trunkSize-=map[key].length;
-		map[key].clean();
+        
+		map[key].clear();
 		map[key].destroy();
 		map.remove(key);
+
 		return true;
 	}
-	override void clean()
+
+	override bool flush()
 	{
-		foreach(k,v;map){
-			v.clean();
+		foreach(k,v;map)
+        {
+			v.clear();
 			v.destroy();
 			map.remove(k);
 		}
+
 		map.destroy();
 		trunkNum = 0;
 		trunkSize = 0;
 		head = null;
 		tail = null;
+
+        return true;
 	}
 }
 
 unittest
 {
 	import std.stdio;
-	Memory memory = new Memory();
+	auto memory = new MemoryCache();
 	string test = "test";
 	ubyte[] utest = cast(ubyte[])test;
 	memory.set(test,utest);
@@ -105,9 +159,9 @@ unittest
 	assert(memory.getTrunkSize == utest.length + utest2.length);
 	assert(memory.get(test2) == utest2);
 	assert(memory.get("testsdfadf") == null);
-	memory.remove(test2);
+	memory.erase(test2);
 	assert(memory.getTrunkNum == 1);
 	assert(memory.getTrunkSize == utest.length);
-	memory.clean();
+	memory.flush();
 	assert(memory.getTrunkNum == 0);
 }
---
+++ source/hunt/cache/driver/package.d
@@ -0,0 +1,6 @@
+module hunt.cache.driver;
+
+public import hunt.cache.driver.base;
+public import hunt.cache.driver.memory;
+public import hunt.cache.driver.memcache;
+public import hunt.cache.driver.redis;
