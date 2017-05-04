b5f81690dcd23cc7cbc98f722e27144f46449561
shove <shove@192.168.0.108>
2017-05-05 01:33:17 +0800

    Replace the staticfile cache method with Application.cache()

--- source/hunt/application/staticfile.d
+++ source/hunt/application/staticfile.d
@@ -21,7 +21,7 @@ class StaticfileController : Controller
     		return;
     	}
     	
-    	ubyte[] content = StaticfileCache.instance.getCache(request.path);
+    	ubyte[] content = Application.getInstance.cache().get!(ubyte[])(request.path);
 
     	if (content != null)
     	{
@@ -61,76 +61,7 @@ class StaticfileController : Controller
 		}
 
     	content = cast(ubyte[])read(staticFilename);
-	    StaticfileCache.instance.setCache(request.path, content);
+	    Application.getInstance.cache().set(request.path, content, Config.app.application.staticFileCacheMinutes);
 		response.setContext(content);
     }
-}
-
-class StaticfileCache
-{
-	__gshared static StaticfileCache instance;
-	__gshared private FileContent[string] _contents;
-
-	static this()
-	{
-		if (instance is null)
-		{
-			instance = new StaticfileCache();
-		}
-	}
-
-	private class FileContent
-	{
-		ubyte[] content;
-		SysTime cacheTime;
-		
-		this(ubyte[] content)
-		{
-			this.content = content;
-			this.cacheTime = Clock.currTime();
-		}
-	}
-
-	ubyte[] getCache(string key)
-	{
-		FileContent fc = _contents.get(key, null);
-
-		if ((fc is null) || ((Clock.currTime() - fc.cacheTime).total!"minutes" > Config.app.application.staticFileCacheMinutes))
-		{
-			return null;
-		}
-
-		return fc.content;
-	}
-	
-	void setCache(string key, ubyte[] content)
-	{
-		if (_contents.length >= Config.app.application.staticFileCacheMaxFileNum)
-		{
-			cleanCache();
-		}
-		
-		if (_contents.length >= Config.app.application.staticFileCacheMaxFileNum)
-		{
-			trace("Static file cache has reached the maximum number of files.");
-			
-			return;
-		}
-		
-		_contents[key] = new FileContent(content);
-	}
-	
-	// clean invalid cache data.
-	void cleanCache()
-	{
-		SysTime t = Clock.currTime();
-		
-		foreach(key, fc; _contents)
-		{
-			if ((t - fc.cacheTime).total!"minutes" >= Config.app.application.staticFileCacheMinutes)
-			{
-				_contents.remove(key);
-			}
-		}
-	}
 }
