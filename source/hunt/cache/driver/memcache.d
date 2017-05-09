1a835766c7ada31b50c0f67bb10b418b36bbcdfd
viile <dakgzhu@foxmail.com>
2017-05-09 19:15:53 +0800

    add cache expire

--- source/hunt/cache/driver/memcache.d
+++ source/hunt/cache/driver/memcache.d
@@ -1,6 +1,63 @@
 module hunt.cache.driver.memcache;
 
-import hunt.cache.driver.base;
-import hunt.storage.memcached;
+import std.conv;
 
-version(USE_MEMCACHE):
+import hunt.cache.driver.base;
+public import driverMemcached = hunt.storage.memcached;
+
+version(USE_MEMCACHE)
+{
+	class MemcacheCache : AbstractCache 
+	{
+		private int _expire;
+		override bool set(string key, ubyte[] value, int expire = 0)
+		{
+			return set(key,value.to!string,expire);
+		}
+
+		override bool set(string key,string value,int expire = 0)
+		{
+			return driverMemcached.Memcache.set(key,value,expire);	
+		}
+		
+		override bool set(string key, ubyte[] value)
+		{
+			return set(key,value,_expire);
+		}
+
+		override bool set(string key,string value)
+		{
+			return set(key,value,_expire);	
+		}
+
+		override string get(string key)
+		{
+			return driverMemcached.Memcache.get(key);
+		}
+
+		T get(T)(string key)
+		{
+			return cast(T)driverMemcached.Memcache.get(key);
+		}
+
+		override bool isset(string key)
+		{
+			return false;	
+		}
+
+		override bool erase(string key)
+		{
+			return false;
+		}
+
+		override bool flush()
+		{
+			return false;
+		}
+
+		override void setExpire(int expire)
+		{
+			this._expire = expire;
+		}
+	}
+}
---
+++ source/hunt/utils/package.d
@@ -0,0 +1,6 @@
+module hunt.utils;
+
+public import hunt.utils.path;
+public import hunt.utils.string;
+public import hunt.utils.url;
+public import hunt.utils.time;
