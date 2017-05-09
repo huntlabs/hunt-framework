13d0d3e1f3a78ed51e439ebad3d5864d8953f081
viile <dakgzhu@foxmail.com>
2017-05-09 19:15:53 +0800

    add cache expire

--- source/hunt/cache/driver/memory.d
+++ source/hunt/cache/driver/memory.d
@@ -1,15 +1,16 @@
 module hunt.cache.driver.memory;
 
 import hunt.cache.driver.base;
+import hunt.utils.time;
 
 import core.memory;
 import core.sync.rwmutex;
-
 class MemoryBuffer
 {
 	string key;
 	ulong length;
 	ubyte[] *ptr;
+	int exprie;
 	MemoryBuffer prv;
 	MemoryBuffer next;
 
@@ -41,6 +42,10 @@ class MemoryCache : AbstractCache
 	private MemoryBuffer head;
 	private MemoryBuffer tail;
 	private ReadWriteMutex _mutex;
+	private int _exprie = 0;
+	private int _counter;
+	//cache check valid cycle
+	private int _checkValidCycle = 100000;
 
 	this()
 	{
@@ -65,11 +70,13 @@ class MemoryCache : AbstractCache
 	override bool set(string key, ubyte[] value, int exprie = 0)
 	{
 		//if(isset(key))return false;
-
 		_mutex.writer.lock();
 		scope(exit) _mutex.writer.unlock();
 
+		if(exprie <= 0)exprie = 0;
+
 		auto trunk = new MemoryBuffer(key,value,value.length);
+		trunk.exprie = (exprie == 0) ? 0 : (getCurrUnixStramp + exprie);
 
 		if(isset(key)){
 			trunkSize = trunkSize + value.length - map[key].length;
@@ -95,20 +102,53 @@ class MemoryCache : AbstractCache
 		return true;
 	}
 
+	override bool set(string key,string value)
+	{
+		return set(key,cast(ubyte[])value,_exprie);
+	}
+	override bool set(string key,string value,int exprie)
+	{
+		return set(key,cast(ubyte[])value,exprie);
+	}
+	override bool set(string key,ubyte[] value)
+	{
+		return set(key,value,_exprie);
+	}
 
-	override ubyte[] get(string key)
+	/*
+	override T get(T)(string key)
 	{
 		_mutex.reader.lock();
 		scope(exit) _mutex.reader.unlock();
 
-		return map.get(key,null) ? *(map[key].ptr) : null;
+		if(!isset(key))return null;
+		if(isExpire(key)){
+			_mutex.reader.unlock();
+			erase(key);
+			return null;
+		}
+		return cast(T)(*(map[key].ptr));
+	}
+	*/
+
+	override string get(string key)
+	{
+		_mutex.reader.lock();
+		//scope(exit) _mutex.reader.unlock();
+
+		if(!isset(key))return null;
+		if(isExpire(key)){
+			_mutex.reader.unlock();
+			erase(key);
+			return null;
+		}
+		_mutex.reader.unlock();
+		return cast(string)(*(map[key].ptr));
 	}
 
 	override bool isset(string key)
 	{
-		if(map.get(key,null) is null) 
-			return false;
-
+		if(map.get(key,null) is null)return false;
 		return true;
 	}
 
@@ -118,7 +158,6 @@ class MemoryCache : AbstractCache
 		scope(exit) _mutex.writer.unlock();
 
 		if(!isset(key))return true;
-
 		if(map[key].prv)map[key].prv.next = map[key].next;
 		if(map[key].next)map[key].next.prv = map[key].prv;
 
@@ -149,24 +188,55 @@ class MemoryCache : AbstractCache
 
 		return true;
 	}
+
+	override void setExpire(int exprie)
+	{
+		this._exprie = exprie;
+	}
+
+	private bool isExpire(string key)
+	{
+		if(map[key].exprie == 0)return true;
+		if(map[key].exprie <= getCurrUnixStramp)
+		{
+			scope(exit)erase(key);
+			return false;
+		}else{
+			return true;
+		}
+
+	}
+
+	private void counter()
+	{
+		_counter++;
+		if(_counter >= _checkValidCycle){
+			foreach(k,v;map)
+			{
+				isExpire(k);
+			}
+			_counter = 0;
+		}
+	}
 }
 
 unittest
 {
 	import std.stdio;
+	import core.thread;
 	auto memory = new MemoryCache();
 	string test = "test";
 	ubyte[] utest = cast(ubyte[])test;
 	memory.set(test,utest);
 	assert(memory.getTrunkNum == 1);
 	assert(memory.getTrunkSize == utest.length);
-	assert(memory.get(test) == utest);
+	assert(memory.get(test) == test);
 	string test2 = "testasdfasjdflkjaklsdjfl";
 	ubyte[] utest2 = cast(ubyte[])test2;
 	memory.set(test2,utest2);
 	assert(memory.getTrunkNum == 2);
 	assert(memory.getTrunkSize == utest.length + utest2.length);
-	assert(memory.get(test2) == utest2);
+	assert(memory.get(test2) == test2);
 	assert(memory.get("testsdfadf") == null);
 	memory.erase(test2);
 	assert(memory.getTrunkNum == 1);
@@ -180,4 +250,20 @@ unittest
 	assert(memory.set(test,utest2) == true);
 	assert(memory.getTrunkNum == 1);
 	assert(memory.getTrunkSize == utest2.length);
+	memory.flush();
+
+	memory.setExpire = 5;
+	assert(memory.set(test,utest,0) == true);
+	assert(memory.set(test2,utest) == true);
+
+	assert(memory.getTrunkNum == 2);
+	assert(memory.getTrunkSize == utest.length + utest.length);
+
+	writeln("wait timeout");
+	Thread.sleep(6.seconds);
+	writeln("timeout");
+
+	assert(memory.get(test2) == null);
+	assert(memory.getTrunkNum == 1);
+	assert(memory.getTrunkSize == utest.length);
 }
--- source/hunt/cache/driver/redis.d
+++ source/hunt/cache/driver/redis.d
@@ -1,3 +1,61 @@
 module hunt.cache.driver.redis;
 
+import std.conv;
+
 import hunt.cache.driver.base;
+public import driverRedis = hunt.storage.redis;
+
+version(USE_REDIS)
+{
+	class RedisCache : AbstractCache 
+	{
+		private int _expire;
+		override bool set(string key, ubyte[] value)
+		{
+			return set(key,value.to!string,_expire);
+		}
+		override bool set(string key, ubyte[] value, int expire = 0)
+		{
+			return set(key,value.to!string,expire);
+		}
+
+		override bool set(string key,string value)
+		{
+			return driverRedis.Redis.set(key,value,_expire);	
+		}
+		override bool set(string key,string value,int expire = 0)
+		{
+			return driverRedis.Redis.set(key,value,expire);	
+		}
+
+		T get(T)(string key)
+		{
+			return cast(T)driverRedis.Redis.get(key);
+		}
+		override string get(string key)
+		{
+			return driverRedis.Redis.get(key);
+		}
+
+		override bool isset(string key)
+		{
+			return driverRedis.Redis.exists(key);
+		}
+
+		override bool erase(string key)
+		{
+			return cast(bool)driverRedis.Redis.del(key);
+		}
+
+		override bool flush()
+		{
+			driverRedis.Redis.flushall();
+			return true;
+		}
+
+		override void setExpire(int expire)
+		{
+			this._expire = expire;
+		}
+	}
+}
