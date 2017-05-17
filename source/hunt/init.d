e96ef174639931b55ad867d3cd3073f9999a2ebd
zoujiaqing <zoujiaqing@gmail.com>
2017-05-17 19:31:03 +0800

    add hunt.init

--- source/hunt/init.d
+++ source/hunt/init.d
@@ -1,17 +1,24 @@
+/*
+ * Hunt - a framework for web and console application based on Collie using Dlang development
+ *
+ * Copyright (C) 2015-2017  Shanghai Putao Technology Co., Ltd
+ *
+ * Developer: HuntLabs
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
+
 module hunt.init;
 
 import std.path : buildPath;
 import std.file : thisExePath;
 
 __gshared string APP_PATH;
-
-__gshared string  DEFAULT_CONFIG_PATH = "config/";
-
-__gshared string  DEFAULT_RESOURCE_PATH = "resources/";
-
-__gshared string  DEFAULT_PUBLIC_PATH = "public/";
-
-__gshared string  DEFAULT_STORAGE_PATH = "storage/";
+__gshared string DEFAULT_CONFIG_PATH = "config/";
+__gshared string DEFAULT_RESOURCE_PATH = "resources/";
+__gshared string DEFAULT_PUBLIC_PATH = "public/";
+__gshared string DEFAULT_STORAGE_PATH = "storage/";
 
 shared static this()
 {
