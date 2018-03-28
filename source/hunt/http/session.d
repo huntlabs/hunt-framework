5bd7c66d8037e9c8d59ad166b57f6fe4bc46b490
zhangyuchun <zhangyuchun@putao.com>
2018-03-28 17:59:05 +0800

    update new cache.

--- source/hunt/http/session.d
+++ source/hunt/http/session.d
@@ -1,6 +1,6 @@
 module hunt.http.session;
 
-import hunt.storage;
+import huntlabs.cache;
 import hunt.utils.time;
 
 import std.json;
@@ -86,44 +86,44 @@ class Session
 
 class SessionStorage
 {
-	this(string driver = "file")
+
+	this(UCache cache)
 	{
-		if(driver == string.init)driver = "file";
-		this._driverName = driver;
-		switch(driver)
-		{
-			case "memory":
-				{
-					this._sessionStorage = new Memory;
-					break;
-				}
-			case "file":
-				{
-					this._sessionStorage = new File;
-					break;
-				}
-				version(USE_MEMCACHE)
-				{
-					case "memcache":
-						{
-							this._sessionStorage = new Memcache;
-							break;
-						}
-				}
-				version(USE_REDIS)
-				{
-					case "redis":
-						{
-							this._sessionStorage = new Redis;
-							break;
-						}
-				}
-			default:
-				{
-					throw new Exception("Can't support cache driver: ", driver);
-				}
-		}
+		_cache = cache;
 	}
+
+
+	alias set = put;
+
+	bool put(string key, string value, int expire)
+	{
+		_cache.put!string(getRealAddr(key), value, expire);
+		return true;
+	}
+	
+	bool put(string key, string value)
+	{
+		return set(key,value,_expire);
+	}
+	
+	string get(string key)
+	{
+		return  cast(string)_cache.get!string(getRealAddr(key));
+	}
+	
+	alias isset = containsKey;
+	bool containsKey(string key)
+	{
+		return _cache.containsKey(key);
+	}
+	
+	alias del = erase;
+	alias remove = erase;
+	bool erase(string key)
+	{
+		return _cache.remove(getRealAddr(key));
+	}
+
 
 	string generateSessionId(string sessionName = "hunt_session")
 	{
@@ -143,88 +143,35 @@ class SessionStorage
 		return str;
 	}
 
-	bool set(string key, string value, int expire)
-	{
-		return _sessionStorage.set(getRealAddr(key), cast(ubyte[])value, expire);
-	}
-
-	bool set(string key, string value)
-	{
-		return set(key,value,_expire);
-	}
-
-	string get(string key)
-	{
-		string str = cast(string)_sessionStorage.get(getRealAddr(key));
-		if("_driverName" == "file"){
-			JSONValue js = parseJSON(str);
-			if(js["_time"].integer <= getCurrUnixStramp){
-				erase(key);
-				return null;
-			}
-		}
-		return str;
-	}
-
-	bool isset(string key)
-	{
-		return _sessionStorage.isset(getRealAddr(key));
-	}
-
-	alias del = erase;
-	alias remove = erase;
-	bool erase(string key)
-	{
-		return _sessionStorage.erase(getRealAddr(key));
-	}
-
-	bool flush()
-	{
-		return _sessionStorage.flush();
-	}
 
 	void setPrefix(string prefix)
 	{
-		this._prefix = prefix;
-	}
-
-	void setPath(string path)
-	{
-		this._path = path;
+		_prefix = prefix;
 	}
 
 	void setExpire(int expire)
 	{
-		this._expire = expire;
-		this._sessionStorage.setExpire(expire);
+		_expire = expire;
 	}
 
 	string getRealAddr(string key)
 	{
-		if(_driverName == "file")
-		{
-			return _path ~ _prefix ~ key;
-		}
 		return _prefix ~ key;
 	}
+
 	int expire()
 	{
 		return _expire;
 	}
 
-	StorageInterface driver()
-	{
-		return _sessionStorage;
-	}
+
 
 	private
 	{
-		string _driverName;
-		string _prefix;
-		string _sessionId;
-		string _path;
-		string _name;
-		int _expire;
-		StorageInterface _sessionStorage;
+		string 		_prefix;
+		string 		_sessionId;
+
+		int 		_expire;
+		UCache 		_cache;
 	}
 }
--- source/hunt/storage/impl/package.d
+++
@@ -1,7 +0,0 @@
-module hunt.storage.impl;
-
-public import hunt.storage.base;
-public import hunt.storage.impl.memory;
-public import hunt.storage.impl.memcache;
-public import hunt.storage.impl.redis;
-public import hunt.storage.impl.file;
