541676b490570229aedef6d1403df361de8fae3e
zoujiaqing <zoujiaqing@gmail.com>
2018-04-11 03:52:05 +0800

    add Authenticate functions

--- source/hunt/security/Auth.d
+++
@@ -1,9 +0,0 @@
-module hunt.security.Auth;
-
-import hunt.security.User;
-
-class Auth
-{
-    private int id;
-    User user;
-}
--- source/hunt/security/permission/Permission.d
+++ source/hunt/security/permission/Permission.d
@@ -1,10 +1,20 @@
 module hunt.security.permission.Permission;
 
-struct Permission
+import hunt.security.permission.Item;
+
+class Permission
 {
-    string group;
-    string key;
-    string routes;
-    string name;
-    string discription;
+    PermissionItem[string] permissions;
+
+    public this(PermissionItem[] permissions)
+    {
+        this.permissions = permissions;
+    }
+
+    public Permission create(string name, string key, string discription)
+    {
+        auto item = new PermissionItem(name, key, discription);
+
+        return this;
+    }
 }
