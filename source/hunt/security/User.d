92ca0fd4864eaefc8a55427daf40b5d8d573c150
zoujiaqing <zoujiaqing@gmail.com>
2018-04-11 03:35:51 +0800

    add security User

--- source/hunt/security/User.d
+++ source/hunt/security/User.d
@@ -4,7 +4,29 @@ import hunt.security.Role;
 
 class User
 {
-    int id;
-    string name;
-    Role role;
+    private
+    {
+        int id;
+        string name;
+        Role role;
+        Role[] roles;
+    }
+
+    public this(int userid, string username, Role role)
+    {
+        this.id = userid;
+        this.name = username;
+        this.role = role;
+    }
+    public int userId()
+    {
+        return this.id;
+    }
+
+    public User assignRole(Role role)
+    {
+        this.roles ~= role;
+        
+        return this;
+    }
 }
