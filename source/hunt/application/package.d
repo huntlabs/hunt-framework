793c3edcab124120907259f64761ef6b50c04b47
Brian Zou <zoujiaqing@gmail.com>
2018-05-08 02:07:25 +0800

    remove model.d from package.d

--- source/hunt/application/package.d
+++ source/hunt/application/package.d
@@ -12,7 +12,6 @@
 module hunt.application;
 
 public import hunt.application.controller;
-public import hunt.application.model;
 public import hunt.application.config;
 public import hunt.application.application;
 public import hunt.application.middleware;
