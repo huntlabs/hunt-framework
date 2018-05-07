182ca80ccb3674df1d624479521201d69a8d4f7e
Brian Zou <zoujiaqing@gmail.com>
2018-05-08 01:49:52 +0800

    Add EntityRepository & CrudRepository to hunt.database package

---
+++ source/hunt/database/repository/EntityRepository.d
@@ -0,0 +1,18 @@
+/*
+ * Entity - Entity is an object-relational mapping tool for the D programming language. Referring to the design idea of JPA.
+ *
+ * Copyright (C) 2015-2018  Shanghai Putao Technology Co., Ltd
+ *
+ * Developer: HuntLabs.cn
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
+ 
+module hunt.database.repository.EntityRepository;
+
+import hunt.database.repository.CrudRepository;
+
+abstract class EntityRepository (T, ID) : CrudRepository!(T, ID)
+{
+}
