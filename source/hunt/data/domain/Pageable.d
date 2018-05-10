bfa323a0db23ec09c3c4525b4ce2b3aa60d19c20
Brian Zou <zoujiaqing@gmail.com>
2018-05-11 04:13:57 +0800

    add data.domain

---
+++ source/hunt/data/domain/Pageable.d
@@ -0,0 +1,36 @@
+module hunt.data.domain.Pageable;
+
+class Pageable
+{
+	static Pageable unpaged() {
+		return null;
+	}
+
+	boolean isPaged() {
+		return true;
+	}
+
+	boolean isUnpaged() {
+		return !isPaged();
+	}
+
+	int getPageNumber();
+
+	int getPageSize();
+
+	long getOffset();
+
+	Sort getSort();
+
+	Sort getSortOr(Sort sort) {
+		return getSort().isSorted() ? getSort() : sort;
+	}
+
+	Pageable next();
+
+	Pageable previousOrFirst();
+
+	Pageable first();
+
+	boolean hasPrevious();
+}
