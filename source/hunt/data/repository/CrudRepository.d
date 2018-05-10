56b96c5c758746f3412eec9b0d6304ef91283cce
zhangyuchun <encounterspring@163.com>
2018-05-10 21:02:27 +0800

    add Crud page.

--- source/hunt/data/repository/CrudRepository.d
+++ source/hunt/data/repository/CrudRepository.d
@@ -13,10 +13,19 @@ module hunt.data.repository.CrudRepository;
 
 import entity;
 import entity.EntityManagerFactory;
-
+import hunt;
 public import entity.repository.Repository;
 
-abstract class CrudRepository(T, ID) : Repository!(T, ID)
+struct Page(T)
+{
+	long totalAll;
+	uint pageIndex;
+	uint pageNum;
+	T[] arrData;
+}
+
+
+class CrudRepository(T, ID) : Repository!(T, ID)
 {
     EntityManager createEntityManager()
     {
@@ -33,7 +42,7 @@ abstract class CrudRepository(T, ID) : Repository!(T, ID)
         criteriaQuery.select(builder.count(root));
         
         Long result = cast(Long)(em.createQuery(criteriaQuery).getSingleResult());
-
+		em.close();
         return result.longValue();
     }
 
@@ -79,8 +88,31 @@ abstract class CrudRepository(T, ID) : Repository!(T, ID)
     {
         T entity = this.findById(id);
         
-        return (null != entity);
+        return (entity !is null);
     }
+
+	public Page!T find(uint page , uint num , bool asc = true)
+	{
+		auto em = this.createEntityManager();
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		auto criteriaQuery = builder.createQuery!(T);
+		Root!T root = criteriaQuery.from();
+
+		if(asc == false)
+		{
+			mixin(orderByIDDesc!T());
+		}
+
+		TypedQuery!T typedQuery = em.createQuery(criteriaQuery.select(root));
+		auto ts = typedQuery.setFirstResult( page * num).setMaxResults(num).getResultList();
+		Page!T resPage;
+		resPage.pageNum = num;
+		resPage.pageIndex = page;
+		resPage.arrData = ts;
+		resPage.totalAll = count();
+		em.close();
+		return resPage;
+	}
 
     public T[] findAll()
     {
@@ -102,7 +134,7 @@ abstract class CrudRepository(T, ID) : Repository!(T, ID)
         foreach (id; ids)
         {
             T entity = this.findById(id);
-            if (null != entity)
+            if (entity !is null)
                 entities ~= entity;
         }
 
@@ -122,13 +154,13 @@ abstract class CrudRepository(T, ID) : Repository!(T, ID)
     {
         auto em = this.createEntityManager();
         
-        if (null == em.find!T(entity))
+        if (em.find!T(entity) is null)
         {
             em.persist(entity);
         }
         else
         {
-            entity = em.merge!(T)(entity);
+            em.merge!T(entity);
         }
 
         em.close();
@@ -147,4 +179,12 @@ abstract class CrudRepository(T, ID) : Repository!(T, ID)
 
         return resultList;
     }
+}
+
+
+
+string orderByIDDesc(T)()
+{
+	string code = "criteriaQuery.orderBy(builder.desc(root."~ T.stringof ~"."~ getSymbolsByUDA!(T, PrimaryKey)[0].stringof ~"));";
+	return code;
 }
