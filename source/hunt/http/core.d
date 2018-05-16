abfde79d9e8092e54d48fe89c19f7079e1ca0791
ZhangXuePing <zhangxueping@putao.com>
2018-05-16 09:51:35 +0800

    missing file added

---
+++ source/hunt/http/core.d
@@ -0,0 +1,15 @@
+module hunt.http.core;
+
+class HttpMethod
+{
+    const Head = "HEAD";
+    const Get = "GET";
+    const Post = "POST";
+    const Put = "PUT";
+    const Patch = "PATCH";
+    const Delete = "DELETE";
+    const Purge = "PURGE";
+    const Options = "OPTIONS";
+    const Trace = "TRACE";
+    const Connect = "CONNECT";
+}
