40554043000904d6f91bba6cd85a5968baf8d533
Brian Zou <zoujiaqing@gmail.com>
2018-05-25 02:11:25 +0800

    Update copyright information for 2018.

--- source/hunt/view/package.d
+++ source/hunt/view/package.d
@@ -1,5 +1,15 @@
-module hunt.view;
+/*
+ * Hunt - Hunt is a high-level D Programming Language Web framework that encourages rapid development and clean, pragmatic design. It lets you build high-performance Web applications quickly and easily.
+ *
+ * Copyright (C) 2015-2018  Shanghai Putao Technology Co., Ltd
+ *
+ * Website: www.huntframework.com
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
 
+module hunt.view;
 
 public import hunt.view.ast;
 public import hunt.view.cache;
