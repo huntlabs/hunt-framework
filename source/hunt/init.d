e7627b8291453fd4c3e1b8002a5c38a0849ecf3b
Brian Zou <zoujiaqing@gmail.com>
2018-05-25 01:37:19 +0800

    update init defined, change BinaryFileResponse to DownloadResponse

--- source/hunt/init.d
+++ source/hunt/init.d
@@ -18,26 +18,23 @@ import std.file : thisExePath;
 
 __gshared string APP_PATH;
 __gshared string DEFAULT_CONFIG_PATH = "config/";
+__gshared string DEFAULT_TEMPLATE_PATH = "views/";
 __gshared string DEFAULT_LANGUAGE_PATH = "resources/languages";
 __gshared string DEFAULT_PUBLIC_PATH = "public/";
 __gshared string DEFAULT_STORAGE_PATH = "storage/";
 __gshared string DEFAULT_LOG_PATH = "logs/";
 __gshared string DEFAULT_SESSION_PATH = "session/";
-__gshared string DEFAULT_Download_Path = "downloads/";
-__gshared string DEFAULT_Views_Path = "views/";
-__gshared string DEFAULT_WWW_Root = "wwwroot/";
+__gshared string DEFAULT_ATTACHMENT_PATH = "attachments/";
 
 shared static this()
 {
     APP_PATH = dirName(thisExePath());
     DEFAULT_CONFIG_PATH = buildPath(APP_PATH, DEFAULT_CONFIG_PATH);
+    DEFAULT_TEMPLATE_PATH = buildPath(APP_PATH, DEFAULT_TEMPLATE_PATH);
     DEFAULT_LANGUAGE_PATH = buildPath(APP_PATH, DEFAULT_LANGUAGE_PATH);
     DEFAULT_PUBLIC_PATH = buildPath(APP_PATH, DEFAULT_PUBLIC_PATH);
     DEFAULT_STORAGE_PATH = buildPath(APP_PATH, DEFAULT_STORAGE_PATH);
     DEFAULT_LOG_PATH = buildPath(DEFAULT_STORAGE_PATH, DEFAULT_LOG_PATH);
     DEFAULT_SESSION_PATH = buildPath(DEFAULT_STORAGE_PATH, DEFAULT_SESSION_PATH);
-    
-    DEFAULT_Download_Path = buildPath(APP_PATH, DEFAULT_Download_Path);
-    DEFAULT_WWW_Root = buildPath(APP_PATH, DEFAULT_WWW_Root);
-    DEFAULT_Views_Path = buildPath(APP_PATH, DEFAULT_Views_Path);
+    DEFAULT_ATTACHMENT_PATH = buildPath(APP_PATH, DEFAULT_ATTACHMENT_PATH);
 }
