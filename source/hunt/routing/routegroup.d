7de15bfdde01924e050fc98b9cc0b44c5017ede8
ZhangXuePing <zhangxueping@putao.com>
2018-05-30 20:15:13 +0800

    defined default static root route

--- source/hunt/routing/routegroup.d
+++ source/hunt/routing/routegroup.d
@@ -19,37 +19,37 @@ import std.string;
 
 class RouteInfo
 {
-	string mca;
-	string path;
-	HTTP_METHODS[] methods;
+    string mca;
+    string path;
+    HTTP_METHODS[] methods;
 
-	this(string mca,string path,HTTP_METHODS[] methods)
-	{
-		this.mca = mca;
-		this.path = path;
-		this.methods = methods;
-	}
+    this(string mca, string path, HTTP_METHODS[] methods)
+    {
+        this.mca = mca;
+        this.path = path;
+        this.methods = methods;
+    }
 }
 
 class RoutePathInfo
 {
-	string path;
-	HTTP_METHODS[] methods;
+    string path;
+    HTTP_METHODS[] methods;
 
-	this(string path,HTTP_METHODS[] methods)
-	{
-		this.path = path;
-		this.methods = methods;
-	}
+    this(string path, HTTP_METHODS[] methods)
+    {
+        this.path = path;
+        this.methods = methods;
+    }
 }
 
 class RouteRegInfo
 {
-	HTTP_METHODS[] methods;
-	this(HTTP_METHODS[] methods)
-	{
-		this.methods = methods;
-	}
+    HTTP_METHODS[] methods;
+    this(HTTP_METHODS[] methods)
+    {
+        this.methods = methods;
+    }
 }
 
 class RouteGroup
@@ -59,108 +59,107 @@ class RouteGroup
         this._name = name;
     }
 
-    public
+    void setName(string name)
     {
-        void setName(string name)
+        this._name = name;
+    }
+
+    string getName()
+    {
+        return this._name;
+    }
+
+    RouteGroup addRoute(Route route)
+    {
+        if (route.getRegular())
         {
-            this._name = name;
+            this._regexRoutes ~= route;
+        }
+        else
+        {
+            //path 
+            this._routes[new RoutePathInfo(route.getPattern(), route.getMethods)] = route;
         }
 
-        string getName()
+        string mca = ((route.getModule()) ? route.getModule() ~ "." : "") ~ route.getController()
+            ~ "." ~ route.getAction();
+
+        this._mcaRoutes[new RouteInfo(mca, route.getPattern, route.getMethods)] = route;
+
+        return this;
+    }
+
+    Route getRoute(string method, string mca)
+    {
+        auto m = getMethod(method);
+        foreach (k, v; _mcaRoutes)
         {
-            return this._name;
+            if (k.mca == mca)
+            {
+                if (canFind(k.methods, m) || k.methods == [HTTP_METHODS.ALL])
+                    return v;
+            }
+        }
+        return null;
+    }
+
+    Route match(string method, string path)
+    {
+        Route route = null;
+
+        auto http_method = getMethod(toUpper(method));
+        foreach (k, v; _routes)
+        {
+            if (k.path == path)
+            {
+                if (canFind(k.methods, http_method) || k.methods == [HTTP_METHODS.ALL])
+                    route = v;
+            }
         }
 
-        RouteGroup addRoute(Route route)
+        if (route)
         {
-            if (route.getRegular())
-            {
-                this._regexRoutes ~= route;
-            }
-            else
-            {
-				//path 
-                this._routes[new RoutePathInfo(route.getPattern(),route.getMethods)] = route;
-            }
-
-            string mca = ((route.getModule()) ? route.getModule() ~ "." : "") ~ route.getController() ~ "." ~ route.getAction();
-
-            this._mcaRoutes[new RouteInfo(mca,route.getPattern,route.getMethods)] = route;
-
-            return this;
+            return route.copy();
         }
-
-        Route getRoute(string method,string mca)
+        else
         {
-			auto m = getMethod(method);
-			foreach(k,v;_mcaRoutes){
-				if(k.mca == mca){
-					if(canFind(k.methods,m) || k.methods == [HTTP_METHODS.ALL])
-						return v;
-				}
-			}
-			return null;
-        }
-
-        Route match(string method,string path)
-        {
-            // TODO: 引用类型使用结构体二次包装
-            Route route = null;
-
-			auto http_method = getMethod(toUpper(method));
-			foreach(k,v;_routes){
-				if(k.path == path){
-					if(canFind(k.methods,http_method) || k.methods == [HTTP_METHODS.ALL])
-						route = v;
-				}
-			}
-
-            if (route)
+            foreach (key, value; this._routes)
             {
-                return route.copy();
-            }
-            else
-            {
-            	foreach(key, value; this._routes)
-            	{
-					import std.string;
-					import std.algorithm.searching;
-            		if (startsWith(path,key.path) && value.staticFilePath.length > 0 && 
-							(canFind(key.methods,http_method) || key.methods == [HTTP_METHODS.ALL]))
-            		{
-            				return value;
-            		}
-            	}
-            }
-            
-            import std.regex;
-            import std.uri : decode;
+                import std.string;
+                import std.algorithm.searching;
 
-            foreach (r; this._regexRoutes)
-            {
-                auto matched = path.match(regex(r.getPattern()));
-				bool matchMethod = false;
-				if(canFind(r.getMethods,http_method) || r.getMethods == [HTTP_METHODS.ALL])
-					matchMethod = true;
-                if (matched && matchMethod)
+                if (startsWith(path, key.path) && value.staticFilePath.length > 0
+                        && (canFind(key.methods, http_method) || key.methods == [HTTP_METHODS.ALL]))
                 {
-                    route = r.copy();
-
-                    string[string] params;
-
-                    foreach(i, key; route.getParamKeys())
-                    {
-                        params[key] = decode(matched.captures[i + 1]);
-                    }
-
-                    route.setParams(params);
-
-                    return route;
+                    return value;
                 }
             }
-
-            return null;
         }
+
+        import std.regex;
+        import std.uri : decode;
+
+        foreach (r; this._regexRoutes)
+        {
+            auto matched = path.match(regex(r.getPattern()));
+            bool matchMethod = false;
+            if (canFind(r.getMethods, http_method) || r.getMethods == [HTTP_METHODS.ALL])
+                matchMethod = true;
+            if (matched && matchMethod)
+            {
+                route = r.copy();
+                string[string] params;
+                foreach (i, key; route.getParamKeys())
+                {
+                    params[key] = decode(matched.captures[i + 1]);
+                }
+
+                route.setParams(params);
+                return route;
+            }
+        }
+
+        return null;
     }
 
     private
