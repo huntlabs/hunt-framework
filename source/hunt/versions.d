9985e112d097fe2c83a8ab2110cf427af1bd3e16
zoujiaqing <zoujiaqing@gmail.com>
2018-07-17 16:48:15 +0800

    upgrade version number to 1.2.0

--- source/hunt/versions.d
+++ source/hunt/versions.d
@@ -15,8 +15,8 @@ import std.conv;
 
 // define hunt framework versions
 enum int HUNT_MAJOR_VERSION = 1;
-enum int HUNT_MINOR_VERSION = 1;
-enum int HUNT_PATCH_VERSION = 1;
+enum int HUNT_MINOR_VERSION = 2;
+enum int HUNT_PATCH_VERSION = 0;
 
 enum HUNT_VERSION = HUNT_MAJOR_VERSION.to!string ~ "." ~ to!string(HUNT_MINOR_VERSION) ~ "." ~ to!string(HUNT_PATCH_VERSION);
 enum XPoweredBy = "Hunt framework v" ~ HUNT_VERSION;
