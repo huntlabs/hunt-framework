22d1e663f9b033afdc093be8d290c02d5c858227
Heromyth <bitworld@qq.com>
2018-11-08 18:45:01 +0800

    WebSocketController updated

--- source/hunt/framework/websocket/WebSocketController.d
+++ source/hunt/framework/websocket/WebSocketController.d
@@ -1,9 +1,10 @@
 module hunt.framework.websocket.WebSocketController;
 
+import hunt.framework.messaging.simp.annotation.SimpAnnotationMethodMessageHandler;
 
 class WebSocketController {
 
-
+    SimpAnnotationMethodMessageHandler annotationHandler;
 
 }
 
--- source/hunt/framework/websocket/WebSocketControllerHelper.d
+++ source/hunt/framework/websocket/WebSocketControllerHelper.d
@@ -1,7 +1,28 @@
 module hunt.framework.websocket.WebSocketControllerHelper;
 
 import hunt.framework.websocket.WebSocketController;
+import hunt.framework.messaging.simp.annotation.SimpAnnotationMethodMessageHandler;
 
 class WebSocketControllerHelper {
-    WebSocketController[string] controllers;
+    static WebSocketController[string] controllers;
+
+    static void createControllers(SimpAnnotationMethodMessageHandler handler) {
+
+    }
+
+    WebSocketController createController(T)(SimpAnnotationMethodMessageHandler handler) {
+        T t = new T();
+        t.annotationHandler = handler;
+
+        return t;
+    }
+
+
+    static void registerController(T)(SimpAnnotationMethodMessageHandler handler) {
+        T t = new T();
+        t.annotationHandler = handler;
+
+        controllers[T.stringof] = t;
+        // return t;
+    }
 }
