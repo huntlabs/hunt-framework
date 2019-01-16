e8d3c922721fb729b797f629568b10fea7635b7d
zoujiaqing <zoujiaqing@gmail.com>
2019-01-16 20:45:01 +0800

    exception to Exception

---
+++ source/hunt/framework/Exception.d
@@ -0,0 +1,22 @@
+﻿/*
+ * Hunt - A high-level D Programming Language Web framework that encourages rapid development and clean, pragmatic design.
+ *
+ * Copyright (C) 2015-2019, HuntLabs
+ *
+ * Website: https://www.huntlabs.net/
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
+
+module hunt.framework.Exception;
+
+import hunt.Exceptions;
+
+mixin ExceptionBuild!("Hunt","");
+
+mixin ExceptionBuild!("Http","Hunt");
+
+mixin ExceptionBuild!("HttpErro","Http");
+
+mixin ExceptionBuild!("CreateResponse","HttpErro");
