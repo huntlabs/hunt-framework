db96498753c2f92dfce02f83ff4b9a30b69f055d
heromyth <bitworld@qq.com>
2019-03-18 18:02:36 +0800

    Add Redis storage

--- source/hunt/framework/storage/redis/package.d
+++ source/hunt/framework/storage/redis/package.d
@@ -1,15 +1,17 @@
 module hunt.framework.storage.redis;
 
 import redis;
-
 import hunt.framework.Simplify;
+import hunt.logging;
 
-static private Redis _redis;
+private Redis _redis;
 
-Redis redis()
-{
-    if (_redis is null)
-    {
+Redis getRedis() {
+    if (_redis is null) {
+        version (HUNT_DEBUG) {
+            tracef("Create a redis connection with %s:%d, password:%s", config()
+                    .redis.host, config().redis.port, config().redis.password);
+        }
         _redis = new Redis(config().redis.host, config().redis.port, config().redis.password);
     }
 
