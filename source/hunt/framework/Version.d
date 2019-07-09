d648ca8010b5fadd64b222389736d80b597e5ef7
zoujiaqing <zoujiaqing@gmail.com>
2019-07-09 17:58:23 +0800

    Upgrade patch version to 10.

--- source/hunt/framework/Version.d
+++ source/hunt/framework/Version.d
@@ -16,7 +16,7 @@ import std.conv : to;
 // define hunt framework versions
 enum int HUNT_MAJOR_VERSION = 2;
 enum int HUNT_MINOR_VERSION = 1;
-enum int HUNT_PATCH_VERSION = 0;
+enum int HUNT_PATCH_VERSION = 10;
 
 enum HUNT_VERSION = HUNT_MAJOR_VERSION.to!string ~ "." ~ to!string(HUNT_MINOR_VERSION) ~ "." ~ to!string(HUNT_PATCH_VERSION);
 enum XPoweredBy = "Hunt framework v" ~ HUNT_VERSION;
