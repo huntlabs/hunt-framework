bfb068d4d612b384b10c8306b6e8ee4e33dd2cb9
heromyth <bitworld@qq.com>
2020-02-14 17:00:50 +0800

    Merge updates

--- source/hunt/framework/Version.d
+++ source/hunt/framework/Version.d
@@ -16,7 +16,7 @@ import std.conv : to;
 // define hunt framework versions
 enum int HUNT_MAJOR_VERSION = 2;
 enum int HUNT_MINOR_VERSION = 2;
-enum int HUNT_PATCH_VERSION = 8;
+enum int HUNT_PATCH_VERSION = 9;
 
 enum HUNT_VERSION = HUNT_MAJOR_VERSION.to!string ~ "." ~ to!string(HUNT_MINOR_VERSION) ~ "." ~ to!string(HUNT_PATCH_VERSION);
 enum HUNT_X_POWERED_BY = "Hunt framework v" ~ HUNT_VERSION;
