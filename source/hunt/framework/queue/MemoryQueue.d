c069aeb5026de6dc00681c070387f77544c8c25d
heromyth <bitworld@qq.com>
2020-04-08 15:30:07 +0800

    MemoryQueue improved

--- source/hunt/framework/queue/MemoryQueue.d
+++ source/hunt/framework/queue/MemoryQueue.d
@@ -15,9 +15,6 @@ import std.parallelism;
 class MemoryQueue : AbstractQueue {
     private SimpleQueue!(ubyte[])[string] _queueMap;
 
-    this() {
-    }
-
     override void push(string channel, ubyte[] message) {
         synchronized(this) {
             auto itemPtr = channel in _queueMap;
@@ -31,31 +28,30 @@ class MemoryQueue : AbstractQueue {
     }
 
     override void onListen(string channel, QueueMessageListener listener) {
-        if(listeners is null) {
-            return;
+        assert(listeners !is null);
+        SimpleQueue!(ubyte[])* itemPtr;
+
+        synchronized(this) {
+            itemPtr = channel in _queueMap;
+            if(itemPtr is null) {
+                _queueMap[channel] = new SimpleQueue!(ubyte[])();
+                itemPtr = channel in _queueMap;
+            }
+        }
+
+        ubyte[] content = itemPtr.dequeue();
+        version(HUNT_FM_DEBUG) {
+            tracef("channel: %s Message: %s", channel, cast(string)content);
+            // tracef("%(%02X %)", content);
         }
         
-        // foreach(string channel, List!QueueMessageListener channelListeners; listeners) {
-        //     auto itemPtr = channel in _queueMap;
-        //     if(itemPtr is null) {
-        //         _queueMap[channel] = new SimpleQueue!(ubyte[])();
-        //         itemPtr = channel in _queueMap;
-        //     }
-
-        //     ubyte[] content = itemPtr.dequeue();
-        //     version(HUNT_FM_DEBUG) {
-        //         tracef("channel: %s Message: %s", channel, msg.bodyAsString());
-        //         tracef("%(%02X %)", content);
-        //     }
-            
-        //     foreach(QueueMessageListener listener; channelListeners) {
-        //         listener(content);
-        //     }
-        // }
+        listener(content);
     }
 
     override protected void onStop() {
-        _queueMap.clear();
+        synchronized(this) {
+            _queueMap.clear();
+        }
     }
 }
 
